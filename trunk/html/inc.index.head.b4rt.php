bb6499e42bac7d0aef88a5b737a2791869904ec6
msn_exploder <msn_exploder@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-07-16 18:46:34 +0000

    changed UpdateRefresh() in inc.index.head.b4rt.php / inc.index.head.tf.php to conform to W3C standards.

--- trunk/html/inc.index.head.b4rt.php
+++ trunk/html/inc.index.head.b4rt.php
@@ -19,8 +19,8 @@
 *******************************************************************************/
 
 if(! isset($_SESSION['user'])) {
-    header('location: login.php');
-    exit();
+	header('location: login.php');
+	exit();
 }
 
 // =============================================================================
@@ -34,27 +34,27 @@ $transferList = getTransferList();
 ?>
 <html>
 <head>
-    <title><?php echo $cfg["pagetitle"] ?></title>
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
-    <LINK REL="StyleSheet" HREF="themes/<?php echo $cfg["theme"] ?>/style.css" TYPE="text/css">
-    <link rel="alternate" title="Transfer-Stats" href="stats.php?f=rss" type="application/rss+xml">
-    <META HTTP-EQUIV="Pragma" CONTENT="no-cache; charset=<?php echo _CHARSET ?>">
+	<title><?php echo $cfg["pagetitle"] ?></title>
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+	<LINK REL="StyleSheet" HREF="themes/<?php echo $cfg["theme"] ?>/style.css" TYPE="text/css">
+	<link rel="alternate" title="Transfer-Stats" href="stats.php?f=rss" type="application/rss+xml">
+	<META HTTP-EQUIV="Pragma" CONTENT="no-cache; charset=<?php echo _CHARSET ?>">
 <?php
-    if ($cfg['ui_indexrefresh'] != "0") {
-        if(!isset($_SESSION['prefresh']) || ($_SESSION['prefresh'] == true)) {
-            echo "<meta http-equiv=\"REFRESH\" content=\"".$cfg["page_refresh"].";URL=index.php\">";
+	if ($cfg['ui_indexrefresh'] != "0") {
+		if(!isset($_SESSION['prefresh']) || ($_SESSION['prefresh'] == true)) {
+			echo "<meta http-equiv=\"REFRESH\" content=\"".$cfg["page_refresh"].";URL=index.php\">";
 ?>
 <script language="JavaScript">
-    var var_refresh = <?php echo $cfg["page_refresh"] ?>;
-    function UpdateRefresh() {
-        span_refresh.innerHTML = String(var_refresh--);
-        setTimeout("UpdateRefresh();", 1000);
-    }
+	var var_refresh = <?php echo $cfg["page_refresh"] ?>;
+	function UpdateRefresh() {
+		document.getElementById("span_refresh").innerHTML = String(var_refresh--);
+		setTimeout("UpdateRefresh();", 1000);
+	}
 </script>
 <?php
-        }
-    }
+		}
+	}
 ?>
 <script language="JavaScript">
 function ShowDetails(name_file) {
@@ -62,20 +62,20 @@ function ShowDetails(name_file) {
 }
 function StartTorrent(name_file) {
   <?php if ($cfg["showdirtree"]) { ?>
-     window.open (name_file,'_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=700,height=675')
+	 window.open (name_file,'_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=700,height=675')
   <?php } else { ?>
-     window.open (name_file,'_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=700,height=555')
+	 window.open (name_file,'_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=700,height=555')
   <?php } ?>
 }
 function ConfirmDelete(file) {
-    return confirm("<?php echo _ABOUTTODELETE ?>: " + file)
+	return confirm("<?php echo _ABOUTTODELETE ?>: " + file)
 }
 </script>
 <style>
-    form {margin: 0px; padding: 0px; display: inline;}
+	form {margin: 0px; padding: 0px; display: inline;}
 </style>
 <?php
 if ($cfg["enable_sorttable"] != 0)
-    include('inc.index.sorttable.php');
+	include('inc.index.sorttable.php');
 ?>
 </head>
--- trunk/html/inc.index.head.tf.php
+++ trunk/html/inc.index.head.tf.php
@@ -5,26 +5,26 @@
 *  www.torrentflux.com
 **************************************************************/
 /*
-    This file is part of TorrentFlux.
+	This file is part of TorrentFlux.
 
-    TorrentFlux is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	TorrentFlux is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    TorrentFlux is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	TorrentFlux is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with TorrentFlux; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	You should have received a copy of the GNU General Public License
+	along with TorrentFlux; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 if(! isset($_SESSION['user'])) {
-    header('location: login.php');
-    exit();
+	header('location: login.php');
+	exit();
 }
 
 // =============================================================================
@@ -38,38 +38,38 @@ $transferList = getDirList($cfg["torrent_file_path"]);
 ?>
 <html>
 <head>
-    <title><?php echo $cfg["pagetitle"] ?></title>
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
-    <LINK REL="StyleSheet" HREF="themes/<?php echo $cfg["theme"] ?>/style.css" TYPE="text/css">
-    <link rel="alternate" title="Transfer-Stats" href="stats.php?f=rss" type="application/rss+xml">
-    <META HTTP-EQUIV="Pragma" CONTENT="no-cache; charset=<?php echo _CHARSET ?>">
+	<title><?php echo $cfg["pagetitle"] ?></title>
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+	<LINK REL="StyleSheet" HREF="themes/<?php echo $cfg["theme"] ?>/style.css" TYPE="text/css">
+	<link rel="alternate" title="Transfer-Stats" href="stats.php?f=rss" type="application/rss+xml">
+	<META HTTP-EQUIV="Pragma" CONTENT="no-cache; charset=<?php echo _CHARSET ?>">
 <?php
-    if ($cfg['ui_indexrefresh'] != "0") {
-        if(!isset($_SESSION['prefresh']) || ($_SESSION['prefresh'] == true)) {
-            echo "<meta http-equiv=\"REFRESH\" content=\"".$cfg["page_refresh"].";URL=index.php\">";
+	if ($cfg['ui_indexrefresh'] != "0") {
+		if(!isset($_SESSION['prefresh']) || ($_SESSION['prefresh'] == true)) {
+			echo "<meta http-equiv=\"REFRESH\" content=\"".$cfg["page_refresh"].";URL=index.php\">";
 ?>
 <script language="JavaScript">
-    var var_refresh = <?php echo $cfg["page_refresh"] ?>;
-    function UpdateRefresh() {
-        span_refresh.innerHTML = String(var_refresh--);
-        setTimeout("UpdateRefresh();", 1000);
-    }
+	var var_refresh = <?php echo $cfg["page_refresh"] ?>;
+	function UpdateRefresh() {
+		document.getElementById("span_refresh").innerHTML = String(var_refresh--);
+		setTimeout("UpdateRefresh();", 1000);
+	}
 </script>
 <?php
-        }
-    }
+		}
+	}
 ?>
 <div id="overDiv" style="position:absolute;visibility:hidden;z-index:1000;"></div>
 <script language="JavaScript">
-    var ol_closeclick = "1";
-    var ol_close = "<font color=#ffffff><b>X</b></font>";
-    var ol_fgclass = "fg";
-    var ol_bgclass = "bg";
-    var ol_captionfontclass = "overCaption";
-    var ol_closefontclass = "overClose";
-    var ol_textfontclass = "overBody";
-    var ol_cap = "&nbsp;Torrent Status";
+	var ol_closeclick = "1";
+	var ol_close = "<font color=#ffffff><b>X</b></font>";
+	var ol_fgclass = "fg";
+	var ol_bgclass = "bg";
+	var ol_captionfontclass = "overCaption";
+	var ol_closefontclass = "overClose";
+	var ol_textfontclass = "overBody";
+	var ol_cap = "&nbsp;Torrent Status";
 </script>
 <script src="overlib.js" type="text/javascript"></script>
 <script language="JavaScript">
@@ -81,23 +81,23 @@ function StartTorrent(name_file)
 {
   <?php if ($cfg["showdirtree"])
   { ?>
-     window.open (name_file,'_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=700,height=675')
+	 window.open (name_file,'_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=700,height=675')
   <?php }
   else
   { ?>
-     window.open (name_file,'_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=700,height=555')
+	 window.open (name_file,'_blank','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=700,height=555')
   <?php } ?>
 }
 function ConfirmDelete(file)
 {
-    return confirm("<?php echo _ABOUTTODELETE ?>: " + file)
+	return confirm("<?php echo _ABOUTTODELETE ?>: " + file)
 }
 </script>
 <style>
-    form {margin: 0px; padding: 0px; display: inline;}
+	form {margin: 0px; padding: 0px; display: inline;}
 </style>
 <?php
 if ($cfg["enable_sorttable"] != 0)
-    include('inc.index.sorttable.php');
+	include('inc.index.sorttable.php');
 ?>
 </head>
