4e5192d2fb3089977c5fa1440e29aa72ce47a327
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-07-25 21:13:49 +0000

    TransferProfile

--- trunk/sql/mysql/mysql_update_Current-Release.to.SVN-HEAD.sql
+++ trunk/sql/mysql/mysql_update_Current-Release.to.SVN-HEAD.sql
@@ -3,6 +3,27 @@
 -- -----------------------------------------------------------------------------
 
 --
+-- tf_trprofiles
+--
+CREATE TABLE tf_trprofiles (
+  id MEDIUMINT(8) NOT NULL auto_increment,
+  name VARCHAR(255) NOT NULL default '',
+  owner INT(10) NOT NULL default '0',
+  public ENUM('0','1') NOT NULL default '0',
+  rate SMALLINT(4) unsigned NOT NULL default '0',
+  drate SMALLINT(4) unsigned NOT NULL default '0',
+  maxuploads TINYINT(3) unsigned NOT NULL default '0',
+  superseeder ENUM('0','1') NOT NULL default '0',
+  runtime ENUM('True','False') NOT NULL default 'False',
+  sharekill SMALLINT(4) unsigned NOT NULL default '0',
+  minport SMALLINT(5) unsigned NOT NULL default '0',
+  maxport SMALLINT(5) unsigned NOT NULL default '0',
+  maxcons SMALLINT(4) unsigned NOT NULL default '0',
+  rerequest MEDIUMINT(8) unsigned NOT NULL default '0',
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
+
+--
 -- inserts
 --
 INSERT INTO tf_settings VALUES ('tfqmgr_loglevel','0');
--- trunk/sql/sqlite/sqlite_update_Current-Release.to.SVN-HEAD.sql
+++ trunk/sql/sqlite/sqlite_update_Current-Release.to.SVN-HEAD.sql
@@ -8,6 +8,26 @@
 BEGIN TRANSACTION;
 
 --
+-- tf_trprofiles
+--
+CREATE TABLE tf_trprofiles (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255) NOT NULL default '',
+  owner INTEGER(10) NOT NULL default '0',
+  public INTEGER(1) NOT NULL default '0',
+  rate INTEGER(4) NOT NULL default '0',
+  drate INTEGER(4) NOT NULL default '0',
+  maxuploads INTEGER(3) NOT NULL default '0',
+  superseeder INTEGER(1) NOT NULL default '0',
+  runtime VARCHAR(5) NOT NULL default 'False',
+  sharekill INTEGER(4) NOT NULL default '0',
+  minport INTEGER(5) NOT NULL default '0',
+  maxport INTEGER(5) NOT NULL default '0',
+  maxcons INTEGER(4) NOT NULL default '0',
+  rerequest INTEGER(8) NOT NULL default '0'
+) ;
+
+--
 -- inserts
 --
 INSERT INTO tf_settings VALUES ('tfqmgr_loglevel','0');
