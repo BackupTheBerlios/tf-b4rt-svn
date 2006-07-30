75f618682f880986e37466ba963865cb06575416
msn_exploder <msn_exploder@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-07-30 14:09:30 +0000

    use templates in drivespace.php.

--- trunk/html/drivespace.php
+++ trunk/html/drivespace.php
@@ -7,49 +7,41 @@
 *  www.torrentflux.com
 **************************************************************/
 /*
-    This file is part of TorrentFlux.
+	This file is part of TorrentFlux.
 
-    TorrentFlux is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	TorrentFlux is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    TorrentFlux is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	TorrentFlux is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with TorrentFlux; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	You should have received a copy of the GNU General Public License
+	along with TorrentFlux; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-include_once("config.php");
-include_once("functions.php");
+require_once("config.php");
+require_once("functions.php");
+require_once("lib/vlib/vlibTemplate.php");
 
+# create new template
+$tmpl = new vlibTemplate("themes/".$cfg["default_theme"]."/tmpl/drivespace.tmpl");
 
 $result = shell_exec("df -h ".$cfg["path"]);
 $result2 = shell_exec("du -sh ".$cfg["path"]."*");
 
+# Nothing without set some vars
+$tmpl->setvar('displayDriveSpaceBar', displayDriveSpaceBar(getDriveSpace($cfg["path"])));
+$tmpl->setvar('main_bgcolor', $cfg["main_bgcolor"]);
+$tmpl->setvar('result', $result);
+$tmpl->setvar('result2', $result2);
+$tmpl->setvar('DisplayHead', DisplayHead(_DRIVESPACE));
+$tmpl->setvar('DisplayFoot', DisplayFoot());
 
-echo DisplayHead(_DRIVESPACE);
-echo "<table width=\"740\" border=0 cellpadding=0 cellspacing=0><tr><td>";
-echo displayDriveSpaceBar(getDriveSpace($cfg["path"]));
-echo "</td></tr></table>";
-?>
-
-<br>
-<div align="left" id="BodyLayer" name="BodyLayer" style="border: thin solid <?php echo $cfg["main_bgcolor"] ?>; position:relative; width:740; height:500; padding-left: 5px; padding-right: 5px; z-index:1; overflow: scroll; visibility: visible">
-
-<?php
-
-echo "<pre>";
-echo $result;
-echo "<br><hr><br>";
-echo $result2;
-echo "</pre>";
-echo "</div>";
-
-echo DisplayFoot();
-
+# lets parse the hole thing
+$tmpl->pparse();
 ?>
