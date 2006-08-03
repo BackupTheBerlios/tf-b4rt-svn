7b187616d2b95b00720780cda3050d097313428c
msn_exploder <msn_exploder@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-08-03 13:30:48 +0000

    updated admin_updateUser.php to use templates.

--- trunk/html/admin_updateUser.php
+++ trunk/html/admin_updateUser.php
@@ -1,13 +1,16 @@
 <?php
 /* $Id: admin_updateUser.php 102 2006-07-31 05:01:28Z msn_exploder $ */
+$tmpl = new vlibTemplate("themes/".$cfg["default_theme"]."/tmpl/admin_updateUser.tmpl");
 $user_id = strtolower($user_id);
 if (IsUser($user_id) && ($user_id != $org_user_id)) {
-	echo getHead(_ADMINISTRATION);
-	// Admin Menu
-	echo getMenu();
-	echo "<br><div align=\"center\">"._TRYDIFFERENTUSERID."<br><strong>".$user_id."</strong> "._HASBEENUSED."<br><br><br>";
-	echo "[<a href=\"admin.php?op=editUser&user_id=".$org_user_id."\">"._RETURNTOEDIT." ".$org_user_id."</a>]</div><br><br><br>";
-	echo getFoot(true,true);
+	$tmpl->setvar('head', getHead(_ADMINISTRATION));
+	$tmpl->setvar('menu', getMenu());
+	$tmpl->setvar('_TRYDIFFERENTUSERID', _TRYDIFFERENTUSERID);
+	$tmpl->setvar('user_id', $user_id);
+	$tmpl->setvar('_HASBEENUSED', _HASBEENUSED);
+	$tmpl->setvar('org_user_id', $org_user_id);
+	$tmpl->setvar('_RETURNTOEDIT', _RETURNTOEDIT);
+	$tmpl->setvar('foot', getFoot(true,true));
 } else {
 	// Admin is changing id or password through edit screen
 	if(($user_id == $cfg["user"] || $cfg["user"] == $org_user_id) && $pass1 != "") {
@@ -18,4 +21,7 @@ if (IsUser($user_id) && ($user_id != $org_user_id)) {
 	AuditAction($cfg["constants"]["admin"], _EDITUSER.": ".$user_id);
 	header("location: admin.php");
 }
+
+
+$tmpl->pparse();
 ?>
