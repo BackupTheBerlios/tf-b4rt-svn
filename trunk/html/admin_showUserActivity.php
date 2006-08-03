aae1e59c9d006fff9b2b8fecbc5e3c8dcc5096c9
msn_exploder <msn_exploder@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-08-03 01:24:03 +0000

    updated admin_showUserActivity.php to use templates.

--- trunk/html/admin_showUserActivity.php
+++ trunk/html/admin_showUserActivity.php
@@ -1,9 +1,11 @@
 <?php
 /* $Id: admin_showUserActivity.php 102 2006-07-31 05:01:28Z msn_exploder $ */
-echo getHead(_ADMINUSERACTIVITY);
-// Admin Menu
-echo getMenu();
-// display Activity for user
-echo getActivity($min, $user_id, $srchFile, $srchAction);
-echo getFoot(true,true);
+$tmpl = new vlibTemplate("themes/".$cfg["default_theme"]."/tmpl/admin_showUserActivity.tmpl");
+
+$tmpl->setvar('head', getHead(_ADMINUSERACTIVITY));
+$tmpl->setvar('menu', getMenu());
+$tmpl->setvar('Activity', getActivity($min, $user_id, $srchFile, $srchAction));
+$tmpl->setvar('foot', getFoot(true,true));
+
+$tmpl->pparse();
 ?>
