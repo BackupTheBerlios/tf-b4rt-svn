030798ea82d11f6b26e9331e70ae107b66adefbd
msn_exploder <msn_exploder@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-08-04 19:35:36 +0000

    use templates in downloaddetails.php.

--- trunk/html/downloaddetails.php
+++ trunk/html/downloaddetails.php
@@ -7,26 +7,29 @@
 *  www.torrentflux.com
 **************************************************************/
 /*
-    This file is part of TorrentFlux.
+	This file is part of TorrentFlux.
 
-    TorrentFlux is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	TorrentFlux is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    TorrentFlux is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	TorrentFlux is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with TorrentFlux; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	You should have received a copy of the GNU General Public License
+	along with TorrentFlux; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-include_once("config.php");
-include_once("functions.php");
-include_once("AliasFile.php");
+require_once("config.php");
+require_once("functions.php");
+require_once("AliasFile.php");
+require_once("lib/vlib/vlibTemplate.php");
+
+$tmpl = new vlibTemplate("themes/".$cfg["default_theme"]."/tmpl/downloaddetails.tmpl");
 
 $torrent = getRequestVar('torrent');
 $error = "";
@@ -35,13 +38,13 @@ $graph_width = "";
 $background = "#000000";
 $alias = getRequestVar('alias');
 if (!empty($alias)) {
-    // create AliasFile object
-    $af = AliasFile::getAliasFileInstance($cfg["torrent_file_path"].$alias, $torrentowner, $cfg);
-    for ($inx = 0; $inx < sizeof($af->errors); $inx++) {
-        $error .= "<li style=\"font-size:10px;color:#ff0000;\">".$af->errors[$inx]."</li>";
-    }
+	// create AliasFile object
+	$af = AliasFile::getAliasFileInstance($cfg["torrent_file_path"].$alias, $torrentowner, $cfg);
+	for ($inx = 0; $inx < sizeof($af->errors); $inx++) {
+		$error .= "<li style=\"font-size:10px;color:#ff0000;\">".$af->errors[$inx]."</li>";
+	}
 } else {
-    die("fatal error torrent file not specified");
+	die("fatal error torrent file not specified");
 }
 
 // Load saved settings
@@ -72,153 +75,86 @@ $label_peers = "";
 $label_maxcons = "";
 $label_sharing = $sharing . '%';
 if ($cfg["sharekill"] != 0)
-    $label_sharekill = $cfg["sharekill"] . '%';
+	$label_sharekill = $cfg["sharekill"] . '%';
 else
-    $label_sharekill = '&#8734';
+	$label_sharekill = '&#8734';
 if (($af->running == 1) && ($alias != "")) {
-    $label_downTotalCurrent = formatFreeSpace($downTotalCurrent / 1048576);
-    $label_upTotalCurrent = formatFreeSpace($upTotalCurrent / 1048576);
-    $label_seeds = $af->seeds;
-    $label_peers = $af->peers;
-    $torrent_pid = getTorrentPid($alias);
-    $torrent_port = netstatPortByPid($torrent_pid);
-    $torrent_cons = netstatConnectionsByPid($torrent_pid);
-    if ($cfg["max_download_rate"] != 0)
-        $label_max_download_rate = " (".number_format($cfg["max_download_rate"], 2).")";
-    else
-        $label_max_download_rate = ' (&#8734)';
-    if ($cfg["max_upload_rate"] != 0)
-        $label_max_upload_rate = " (".number_format($cfg["max_upload_rate"], 2).")";
-    else
-        $label_max_upload_rate = ' (&#8734)';
-    $label_maxcons = " (".$cfg["maxcons"].")";
+	$label_downTotalCurrent = formatFreeSpace($downTotalCurrent / 1048576);
+	$label_upTotalCurrent = formatFreeSpace($upTotalCurrent / 1048576);
+	$label_seeds = $af->seeds;
+	$label_peers = $af->peers;
+	$torrent_pid = getTorrentPid($alias);
+	$torrent_port = netstatPortByPid($torrent_pid);
+	$torrent_cons = netstatConnectionsByPid($torrent_pid);
+	if ($cfg["max_download_rate"] != 0)
+		$label_max_download_rate = " (".number_format($cfg["max_download_rate"], 2).")";
+	else
+		$label_max_download_rate = ' (&#8734)';
+	if ($cfg["max_upload_rate"] != 0)
+		$label_max_upload_rate = " (".number_format($cfg["max_upload_rate"], 2).")";
+	else
+		$label_max_upload_rate = ' (&#8734)';
+	$label_maxcons = " (".$cfg["maxcons"].")";
 }
-
 if ($af->percent_done < 0) {
-    $af->percent_done = round(($af->percent_done*-1)-100,1);
-    $af->time_left = _INCOMPLETE;
+	$af->percent_done = round(($af->percent_done*-1)-100,1);
+	$af->time_left = _INCOMPLETE;
 }
-
 if($af->percent_done < 1)
-    $graph_width = "1";
+	$graph_width = "1";
 else
-    $graph_width = $af->percent_done;
-
+	$graph_width = $af->percent_done;
 if($af->percent_done >= 100) {
-    $af->percent_done = 100;
-    $background = "#0000ff";
+	$af->percent_done = 100;
+	$background = "#0000ff";
 }
-
 $torrentLabel = $torrent;
 if(strlen($torrentLabel) >= 39)
-    $torrentLabel = substr($torrent, 0, 35)."...";
-
+	$torrentLabel = substr($torrent, 0, 35)."...";
 $hd = getStatusImage($af);
-
-echo getHead(_DOWNLOADDETAILS, false, "5", $af->percent_done."% ");
-
-?>
-    <div align="center">
-    <table width="100%" cellpadding="0" cellspacing="0" border="0">
-    <tr>
-        <td align="center">
-<?php
-    if ($error != "")
-    {
-        echo "<img src=\"images/error.gif\" width=16 height=16 border=0 title=\"ERROR\" align=\"absmiddle\">";
-    }
-    echo $torrentLabel."<font class=\"tiny\"> (".formatBytesToKBMGGB($af->size).")</font>";
-?>
-        </td>
-        <!-- b4rt+b4rt-2 -->
-        <td align="right" width="16">
-        <?php
-        if ($af->running == 1)
-        	echo "<a href=\"downloadhosts.php?torrent=".$torrent."&alias=".$alias."\">";
-        echo "<img src=\"images/".$hd->image."\" width=\"16\" height=\"16\" border=\"0\" title=\"".$hd->title."\">";
-        if ($af->running == 1)
-        	echo "</a>";
-        ?>
-        </td>
-        <!-- b4rt+b4rt-2 -->
-    </tr>
-    </table> <!-- 352 -->
-    <table bgcolor="<?php echo $cfg["table_header_bg"] ?>" width="352" cellpadding="1">
-     <tr>
-         <td>
-        <table width="100%" cellpadding="0" cellspacing="0" border="0">
-        <tr>
-            <td background="themes/<?php echo $cfg["theme"] ?>/images/proglass.gif"><img src="images/blank.gif" width="<?php echo $graph_width * 3.5 ?>" height="13" border="0"></td>
-            <td background="themes/<?php echo $cfg["theme"] ?>/images/noglass.gif" bgcolor="<?php echo $background ?>"><img src="images/blank.gif" width="<?php echo (100 - $graph_width) * 3.5 ?>" height="13" border="0"></td>
-        </tr>
-        </table>
-        </td>
-     </tr>
-     <tr><td>
-        <div align="center">
-        <table border="0" cellpadding="2" cellspacing="2" width="90%">
-        <tr>
-            <td align="right"><div class="tiny"><?php echo _ESTIMATEDTIME ?>:</div></td>
-            <td colspan="3" bgcolor="<?php echo $cfg["body_data_bg"] ?>"><div class="tiny"><?php echo "<strong>".$af->time_left."</strong>" ?></div></td>
-        </tr>
-        <tr>
-            <td align="right"><div class="tiny"><?php echo _PERCENTDONE ?>:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$af->percent_done."%</strong>" ?></div></td>
-            <td align="right"><div class="tiny"><?php echo _USER ?>:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$torrentowner."</strong>" ?></div></td>
-        </tr>
-        <tr>
-            <td align="right"><div class="tiny"><?php echo _DOWNLOADSPEED ?>:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$af->down_speed.$label_max_download_rate."</strong>" ?></div></td>
-            <td align="right"><div class="tiny"><?php echo _UPLOADSPEED ?>:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$af->up_speed.$label_max_upload_rate."</strong>" ?></div></td>
-        </tr>
-        <tr>
-            <td align="right"><div class="tiny">Down:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$label_downTotalCurrent."</strong>" ?></div></td>
-            <td align="right"><div class="tiny">Up:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$label_upTotalCurrent."</strong>" ?></div></td>
-        </tr>
-        <tr>
-            <td align="right"><div class="tiny">Down-Total:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$label_downTotal."</strong>" ?></div></td>
-            <td align="right"><div class="tiny">Up-Total:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$label_upTotal."</strong>" ?></div></td>
-        </tr>
-        <tr>
-            <td align="right"><div class="tiny">Seeds:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$label_seeds."</strong>" ?></div></td>
-            <td align="right"><div class="tiny">Peers:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$label_peers."</strong>" ?></div></td>
-        </tr>
-        <tr>
-            <td align="right"><div class="tiny"><?php echo _ID_PORT ?>:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$torrent_port."</strong>" ?></div></td>
-            <td align="right"><div class="tiny"><?php echo _ID_CONNECTIONS ?>:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$torrent_cons.$label_maxcons."</strong>" ?></div></td>
-        </tr>
-        <tr>
-            <td align="right"><div class="tiny"><?php echo _SHARING ?>:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$label_sharing."</strong>" ?></div></td>
-            <td align="right"><div class="tiny">Seed Until:</div></td>
-            <td bgcolor="<?php echo $cfg["body_data_bg"] ?>" nowrap><div class="tiny"><?php echo "<strong>".$label_sharekill."</strong>" ?></div></td>
-        </tr>
-<?php
-    if ($error != "")
-    {
-?>
-        <tr>
-            <td align="right" valign="top"><div class="tiny">Error(s):</div></td>
-            <td colspan="3" width="66%"><div class="tiny"><?php echo "<strong class=\"tiny\">".$error."</strong>" ?></div></td>
-        </tr>
-<?php
-    }
-?>
-        </table>
-    </div>
-</td></tr></table>
-<?php
-
-echo getFoot(false);
-
+$tmpl->setvar('head', getHead(_DOWNLOADDETAILS, false, "5", $af->percent_done."% "));
+if ($error != "") {
+	$tmpl->setvar('is_error', 1);
+	$tmpl->setvar('error', $error);
+}
+$tmpl->setvar('torrentLabel', $torrentLabel);
+$tmpl->setvar('formatBytesToKBMGGB', formatBytesToKBMGGB($af->size));
+if ($af->running == 1) {
+	$tmpl->setvar('running', 1);
+	$tmpl->setvar('torrent', $torrent);
+	$tmpl->setvar('alias', $alias);
+}
+$tmpl->setvar('hd_image', $hd->image);
+$tmpl->setvar('hd_title', $hd->title);
+$tmpl->setvar('table_header_bg', $cfg["table_header_bg"]);
+$tmpl->setvar('theme', $cfg["theme"]);
+$tmpl->setvar('background', $background);
+$tmpl->setvar('graph_width1', $graph_width * 3.5);
+$tmpl->setvar('graph_width2', (100 - $graph_width) * 3.5);
+$tmpl->setvar('_ESTIMATEDTIME', _ESTIMATEDTIME);
+$tmpl->setvar('body_data_bg', $cfg["body_data_bg"]);
+$tmpl->setvar('time_left', $af->time_left);
+$tmpl->setvar('_PERCENTDONE', _PERCENTDONE);
+$tmpl->setvar('percent_done', $af->percent_done);
+$tmpl->setvar('_USER', _USER);
+$tmpl->setvar('torrentowner', $torrentowner);
+$tmpl->setvar('_DOWNLOADSPEED', _DOWNLOADSPEED);
+$tmpl->setvar('down_speed', $af->down_speed.$label_max_download_rate);
+$tmpl->setvar('_UPLOADSPEED', _UPLOADSPEED);
+$tmpl->setvar('up_speed', $af->up_speed.$label_max_upload_rate);
+$tmpl->setvar('downTotalCurrent', $label_downTotalCurrent);
+$tmpl->setvar('upTotalCurrent', $label_upTotalCurrent);
+$tmpl->setvar('downTotal', $label_downTotal);
+$tmpl->setvar('upTotal', $label_upTotal);
+$tmpl->setvar('seeds', $label_seeds);
+$tmpl->setvar('peers', $label_peers);
+$tmpl->setvar('_ID_PORT', _ID_PORT);
+$tmpl->setvar('port', $torrent_port);
+$tmpl->setvar('_ID_CONNECTIONS', _ID_CONNECTIONS);
+$tmpl->setvar('cons', $torrent_cons.$label_maxcons);
+$tmpl->setvar('_SHARING', _SHARING);
+$tmpl->setvar('label_sharing', $label_sharing);
+$tmpl->setvar('label_sharekill', $label_sharekill);
+$tmpl->setvar('foot', getFoot(false));
+$tmpl->pparse();
 ?>
