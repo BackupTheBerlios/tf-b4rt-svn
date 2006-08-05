d79f05e9bd9f7f71ca40b55d05ef5e8d3bf19b31
msn_exploder <msn_exploder@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-08-05 18:29:48 +0000

    use templates in mrtg.php.

--- trunk/html/mrtg.php
+++ trunk/html/mrtg.php
@@ -23,8 +23,11 @@
 
 define('_DEFAULT_TARGET','traffic');
 
-include_once("config.php");
-include_once("functions.php");
+require_once("config.php");
+require_once("functions.php");
+require_once("lib/vlib/vlibTemplate.php");
+
+$tmpl = new vlibTemplate("themes/".$cfg["default_theme"]."/tmpl/mrtg.tmpl");
 
 // request-vars
 $mrtgTarget = getRequestVar('mrtg_target');
@@ -40,15 +43,15 @@ if ($dirHandle = opendir('./mrtg')) {
   $htmlTargets .= '<select name="mrtg_target" size="1" onChange="submit();">';
   $idx = 0;
   while (false !== ($file = readdir($dirHandle))) {
-    if( preg_match("/.*inc/i", $file) ) {
-      $mrtgTargets[$idx] = $file;
-      $targetName = array_shift(explode('.',$file));
-      $htmlTargets .= '<option value="'.$targetName.'"';
-      if ($mrtgTarget == $targetName)
-        $htmlTargets .= ' selected';
-      $htmlTargets .= '>'.$targetName.'</option>';
-      $idx++;
-    }
+	if( preg_match("/.*inc/i", $file) ) {
+	  $mrtgTargets[$idx] = $file;
+	  $targetName = array_shift(explode('.',$file));
+	  $htmlTargets .= '<option value="'.$targetName.'"';
+	  if ($mrtgTarget == $targetName)
+		$htmlTargets .= ' selected';
+	  $htmlTargets .= '>'.$targetName.'</option>';
+	  $idx++;
+	}
   }
   closedir($dirHandle);
   $htmlTargets .= '</select><input type="submit" value="Change Graph">';
@@ -62,7 +65,7 @@ $filename = "./mrtg/".$mrtgTarget.".inc";
 if (file_exists($filename)) {
   $fileHandle = fopen ($filename, "r");
   while (!feof($fileHandle))
-    $htmlGraph .= fgets($fileHandle, 4096);
+	$htmlGraph .= fgets($fileHandle, 4096);
   fclose ($fileHandle);
   // we are only interested in the "real" content
   $htmlGraph = array_shift(explode("_CONTENT_END_",array_pop(explode("_CONTENT_BEGIN_",$htmlGraph))));
@@ -71,13 +74,13 @@ if (file_exists($filename)) {
 }
 
 // render page content
-echo getHead(_ID_MRTG);
-if ((count($mrtgTargets)) > 0)
-  echo $htmlTargets;
-echo '<div align="center" id="BodyLayer" name="BodyLayer" style="border: thin solid ';
-echo $cfg["main_bgcolor"];
-echo 'position:relative; width:740; height:500; padding-left: 5px; padding-right: 5px; z-index:1; overflow: scroll; visibility: visible">';
-echo $htmlGraph;
-echo '</div>';
-echo getFoot();
+$tmpl->('head', getHead(_ID_MRTG));
+if ((count($mrtgTargets)) > 0) {
+	$tmpl->setvar('htmlTargets', $htmlTargets);
+}
+$tmpl->setvar('main_bgcolor', $cfg["main_bgcolor"]);
+$tmpl->setvar('htmlGraph', $htmlGraph);
+$tmpl->setvar('foot', getFoot());
+
+$tmpl->pparse();
 ?>
