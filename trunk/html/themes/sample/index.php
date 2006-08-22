7a23d717bc44eff10bd4fc7ef1ac7577ffd35f08
msn_exploder <msn_exploder@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-08-22 19:36:37 +0000

    added sample theme. actually it is just BlueFlux quick fixed to use new Template System.

---
+++ trunk/html/themes/sample/index.php
@@ -0,0 +1,21 @@
+<?php
+
+# just used because not every function is using the new templates system
+# this functions should be reworked really shortly
+# and then this file will be obsolete
+# now styles should use css files
+
+/* $Id: index.php 232 2006-08-09 23:33:48Z msn_exploder $ */
+
+$cfg["main_bgcolor"] = "#829FB5";
+$cfg["table_data_bg"] = "#EEEEEE";
+$cfg["table_border_dk"] = "#d6dfe6";
+$cfg["table_header_bg"] = "#d6dfe6";
+$cfg["table_admin_border"] = "#FFFFFF";
+$cfg["body_data_bg"] = "#d6dfe6";
+
+// Directory alternating colors for dir.php
+$cfg["bgLight"] = "#ececec";
+$cfg["bgDark"] = "#e1e1e1";
+
+?>
