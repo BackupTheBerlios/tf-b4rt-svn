32dd96d85ad13bf2d50f10e3586dbd32578cce16
msn_exploder <msn_exploder@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-08-25 19:10:59 +0000

    small fix due themes rework.

--- trunk/html/db.php
+++ trunk/html/db.php
@@ -44,7 +44,7 @@ function showError($db, $sql) {
 	if($db->ErrorNo() != 0) {
 		$tmpl = new vlibTemplate("themes/old_style_themes/tmpl/db.tmpl");
 		$tmpl->setvar('error', 1);
-		include("themes/matrix/index.php");
+		include("themes/old_style_themes/matrix/index.php");
 		$tmpl->setvar('pagetitle', $cfg["pagetitle"]);
 		$tmpl->setvar('main_bgcolor', $cfg["main_bgcolor"]);
 		$tmpl->setvar('table_border_dk', $cfg["table_border_dk"]);
