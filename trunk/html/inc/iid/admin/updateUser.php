5d6d5256771f708cf85db04865523f997394b168
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-09-14 20:04:43 +0000

    cleanup : admin/updateUser

--- trunk/html/inc/iid/admin/updateUser.php
+++ trunk/html/inc/iid/admin/updateUser.php
@@ -20,34 +20,46 @@
 
 *******************************************************************************/
 
-// create template-instance
-$tmpl = getTemplateInstance($cfg["theme"], "admin/updateUser.tmpl");
-
+$user_id = getRequestVar('user_id');
+$org_user_id = getRequestVar('org_user_id');
+$pass1 = getRequestVar('pass1');
+$userType = getRequestVar('userType');
+$hideOffline = getRequestVar('hideOffline');
 $user_id = strtolower($user_id);
-if (IsUser($user_id) && ($user_id != $org_user_id)) {
-	$tmpl->setvar('head', getHead($cfg['_ADMINISTRATION']));
-	$tmpl->setvar('menu', getMenu());
-	$tmpl->setvar('_TRYDIFFERENTUSERID', $cfg['_TRYDIFFERENTUSERID']);
-	$tmpl->setvar('user_id', $user_id);
-	$tmpl->setvar('_HASBEENUSED', $cfg['_HASBEENUSED']);
-	$tmpl->setvar('org_user_id', $org_user_id);
-	$tmpl->setvar('_RETURNTOEDIT', $cfg['_RETURNTOEDIT']);
-	$tmpl->setvar('foot', getFoot(true));
-} else {
+
+if (!(IsUser($user_id) && ($user_id != $org_user_id))) {
 	// Admin is changing id or password through edit screen
-	if(($user_id == $cfg["user"] || $cfg["user"] == $org_user_id) && $pass1 != "") {
+	if (($user_id == $cfg["user"] || $cfg["user"] == $org_user_id) && $pass1 != "") {
 		// this will expire the user
 		$_SESSION['user'] = md5($cfg["pagetitle"]);
 	}
 	updateThisUser($user_id, $org_user_id, $pass1, $userType, $hideOffline);
 	AuditAction($cfg["constants"]["admin"], $cfg['_EDITUSER'].": ".$user_id);
 	header("location: index.php?iid=admin");
+	exit();
 }
+
+// create template-instance
+$tmpl = getTemplateInstance($cfg["theme"], "admin/updateUser.tmpl");
+
+// set vars
+$tmpl->setvar('user_id', $user_id);
+$tmpl->setvar('org_user_id', $org_user_id);
+//
+$tmpl->setvar('_TRYDIFFERENTUSERID', $cfg['_TRYDIFFERENTUSERID']);
+$tmpl->setvar('_HASBEENUSED', $cfg['_HASBEENUSED']);
+$tmpl->setvar('_RETURNTOEDIT', $cfg['_RETURNTOEDIT']);
+//
+$tmpl->setvar('menu', getMenu());
+$tmpl->setvar('head', getHead($cfg['_ADMINISTRATION']));
+$tmpl->setvar('foot', getFoot(true));
 $tmpl->setvar('pagetitle', $cfg["pagetitle"]);
 $tmpl->setvar('theme', $cfg["theme"]);
 $tmpl->setvar('ui_dim_details_w', $cfg["ui_dim_details_w"]);
 $tmpl->setvar('ui_dim_details_h', $cfg["ui_dim_details_h"]);
 $tmpl->setvar('iid', $_GET["iid"]);
+
+// parse template
 $tmpl->pparse();
 
 ?>
