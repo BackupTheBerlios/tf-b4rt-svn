abd549b2066289d416f5ae5bc24451ac697d2778
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-09-14 11:39:45 +0000

    cleanup : all_services

--- trunk/html/inc/iid/all_services.php
+++ trunk/html/inc/iid/all_services.php
@@ -26,31 +26,27 @@ require_once('inc/functions/functions.common.php');
 // create template-instance
 $tmpl = getTemplateInstance($cfg["theme"], "all_services.tmpl");
 
-$result = shell_exec("df -h ".$cfg["path"]);
-$result2 = shell_exec("du -sh ".$cfg["path"]."*");
-$result4 = shell_exec("w");
-$result5 = shell_exec("free -mo");
-
+// set vars
+$tmpl->setvar('result1', shell_exec("df -h ".$cfg["path"]));
+$tmpl->setvar('result2', shell_exec("du -sh ".$cfg["path"]."*"));
+$tmpl->setvar('result3', shell_exec("w"));
+$tmpl->setvar('result4', shell_exec("free -mo"));
+$tmpl->setvar('netstatConnectionsSum', netstatConnectionsSum());
+$tmpl->setvar('netstatPortList', netstatPortList());
+$tmpl->setvar('netstatHostList', netstatHostList());
+$tmpl->setvar('_DRIVESPACE', $cfg['_DRIVESPACE']);
+$tmpl->setvar('_ID_HOSTS', $cfg['_ID_HOSTS']);
+$tmpl->setvar('_ID_PORTS', $cfg['_ID_PORTS']);
+$tmpl->setvar('_ID_CONNECTIONS', $cfg['_ID_CONNECTIONS']);
+$tmpl->setvar('_SERVERSTATS', $cfg['_SERVERSTATS']);
 $tmpl->setvar('pagetitle', $cfg["pagetitle"]);
 $tmpl->setvar('theme', $cfg["theme"]);
+$tmpl->setvar('head', getHead($cfg['_ALL']));
+$tmpl->setvar('driveSpaceBar', getDriveSpaceBar(getDriveSpace($cfg["path"])));
+$tmpl->setvar('main_bgcolor', $cfg["main_bgcolor"]);
+$tmpl->setvar('foot', getFoot());
 $tmpl->setvar('ui_dim_details_w', $cfg["ui_dim_details_w"]);
 $tmpl->setvar('ui_dim_details_h', $cfg["ui_dim_details_h"]);
-$tmpl->setvar('head', getHead($cfg['_ALL']));
-$tmpl->setvar('getDriveSpaceBar', getDriveSpaceBar(getDriveSpace($cfg["path"])));
-$tmpl->setvar('main_bgcolor', $cfg["main_bgcolor"]);
-$tmpl->setvar('_DRIVESPACE', $cfg['_DRIVESPACE']);
-$tmpl->setvar('result', $result);
-$tmpl->setvar('result2', $result2);
-$tmpl->setvar('_SERVERSTATS', $cfg['_SERVERSTATS']);
-$tmpl->setvar('result4', $result4);
-$tmpl->setvar('result5', $result5);
-$tmpl->setvar('_ID_CONNECTIONS', $cfg['_ID_CONNECTIONS']);
-$tmpl->setvar('netstatConnectionsSum', netstatConnectionsSum());
-$tmpl->setvar('_ID_PORTS', $cfg['_ID_PORTS']);
-$tmpl->setvar('netstatPortList', netstatPortList());
-$tmpl->setvar('_ID_HOSTS', $cfg['_ID_HOSTS']);
-$tmpl->setvar('netstatHostList', netstatHostList());
-$tmpl->setvar('foot', getFoot());
 $tmpl->setvar('iid', $_GET["iid"]);
 $tmpl->pparse();
 
