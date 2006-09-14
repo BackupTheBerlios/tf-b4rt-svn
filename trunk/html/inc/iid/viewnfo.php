ccf88462a3e69c804c415b1bc337bc7e63e5e642
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-09-14 17:52:33 +0000

    cleanup : viewnfo

--- trunk/html/inc/iid/viewnfo.php
+++ trunk/html/inc/iid/viewnfo.php
@@ -22,36 +22,36 @@
 
 // contributed by NovaKing -- thanks duder!
 
-// common functions
-require_once('inc/functions/functions.common.php');
-
 // create template-instance
 $tmpl = getTemplateInstance($cfg["theme"], "viewnfo.tmpl");
 
-$tmpl->setvar('head', getHead("View NFO"));
-
-$file = $_GET["path"];
-$folder = htmlspecialchars( substr( $file, 0, strrpos( $file, "/" ) ) );
+// set vars
+$file = $_REQUEST["path"];
+$tmpl->setvar('file', $file);
+$folder = htmlspecialchars(substr($file, 0, strrpos($file, "/" )));
 $tmpl->setvar('folder', $folder);
-
-if( ( $output = @file_get_contents( $cfg["path"] . $file ) ) === false ) {
+if ($fileHandle = @fopen($cfg["path"].$file,'r')) {
+	$output = "";
+	while (!@feof($fileHandle))
+		$output .= @fgets($fileHandle, 4096);
+	@fclose ($fileHandle);
+} else {
 	$output = "Error opening NFO File.";
 }
-$tmpl->setvar('file', $file);
-
-if( ( empty( $_GET["dos"] ) && empty( $_GET["win"] ) ) || !empty( $_GET["dos"] ) ) {
-	$tmpl->setvar('output', htmlentities( $output, ENT_COMPAT, "cp866" ));
-}
-else {
+if ((empty($_REQUEST["dos"]) && empty($_REQUEST["win"])) || !empty($_REQUEST["dos"]))
+	$tmpl->setvar('output', htmlentities($output, ENT_COMPAT, "cp866"));
+else
 	$tmpl->setvar('output', htmlentities( $output ));
-}
-
+//
+$tmpl->setvar('head', getHead("View NFO"));
 $tmpl->setvar('foot', getFoot());
 $tmpl->setvar('pagetitle', $cfg["pagetitle"]);
 $tmpl->setvar('theme', $cfg["theme"]);
 $tmpl->setvar('ui_dim_details_w', $cfg["ui_dim_details_w"]);
 $tmpl->setvar('ui_dim_details_h', $cfg["ui_dim_details_h"]);
 $tmpl->setvar('iid', $_GET["iid"]);
+
+// parse template
 $tmpl->pparse();
 
 ?>
