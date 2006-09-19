e7259e726f2005f7d2726b56229d89a3894576ac
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-09-19 20:35:13 +0000

    fixed the javascript-error that was present in form-based login.

--- trunk/html/js/login.js
+++ trunk/html/js/login.js
@@ -2,21 +2,18 @@ function loginvalidate() {
 	msg = "";
 	pass = document.theForm.iamhim.value;
 	user = document.theForm.username.value;
-	if (user.length < 1)
-	{
+	if (user.length < 1) {
 		msg = msg + "* Username is required\n";
 		document.theForm.username.focus();
 	}
-	if(pass.length<1)
-	{
+	if (pass.length <1 ) {
 		msg = msg + "* Password is required\n";
 		if (user.length > 0)
 		{
 			document.theForm.iamhim.focus();
 		}
 	}
-	if (msg != "")
-	{
+	if (msg != "") {
 		alert("Check the following:\n\n" + msg);
 		return false;
 	}
