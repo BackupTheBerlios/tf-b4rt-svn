71c31f0a1ba4b7b12c2dcd08066351f9aa3b3e92
msn_exploder <msn_exploder@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-09-19 18:08:36 +0000

    changes in setup.php (not really working atm).

--- trunk/setup.php
+++ trunk/setup.php
@@ -20,16 +20,19 @@
 
 *******************************************************************************/
 
-# check_binary
-# checks if an binary exists
-# op: name of the binary
-function check_binary($binary, $fatal) {
+/*******************************************************************************
+ START FUNCTIONS
+*******************************************************************************/
+
+// check_binary
+// checks if an binary exists
+// op: name of the binary
+function check_binary($binary) {
 	$shell = shell_exec("which $binary");
 	if (strstr($shell, "no")) {
 		return array(
 			'title' => $binary." NOT found",
 			'status' => 0,
-			'fatal' => $fatal,
 		);
 	}
 	else {
@@ -40,28 +43,27 @@ function check_binary($binary, $fatal) {
 	}
 }
 
-# check_extension
-# checks if a php extensions exists
-# op: name of the extension
-function check_extension($extension, $fatal) {
+// check_extension
+// checks if a php extensions exists
+// op: name of the extension
+function check_extension($extension) {
 	$load_ext = get_loaded_extensions();
 	if (in_array($extension, $load_ext)) {
 		return array(
-			'title' => "php extension ".$extension." found",
+			'title' => "PHP extension ".$extension." found.",
 			'status' => 1,
 		);
 	}
 	return array(
-		'title' => "php extension ".$extension." NOT found",
+		'title' => "PHP extension ".$extension." NOT found.",
 		'status' => 0,
-		'fatal' => $fatal,
 	);
 }
 
-# check_config
-# checks if the php settings are proper
-# op: name of the setting
-function check_config($config, $fatal) {
+// check_config
+// checks if the php settings are proper
+// op: name of the setting
+function check_config($config) {
 	if(!ini_get($config)) {
 		return array(
 			'title' => "Setting ".$config." is proper set.",
@@ -71,14 +73,13 @@ function check_config($config, $fatal) {
 	return array(
 		'title' => "Setting ".$config." is NOT proper set.",
 		'status' => 0,
-		'fatal' => $fatal,
 	);
 }
 
-# display_results
-# create table to display checking results
-# op1: title
-# op2: result
+// display_results
+// create table to display checking results
+// op1: title
+// op2: result
 function display_results($title, $result) {
 	$return = "<tr>";
 	$return .= "<td>";
@@ -91,139 +92,52 @@ function display_results($title, $result) {
 	$return .= "</td>";
 	$return .= "<td>";
 	if ($result['status'] == 1) {
-		$return .= "Done";
+		$return .= "<img src='html/themes/default/images/green.gif'>";
 	}
 	else {
-		$return .= "<b>Failed!</b>";
+		$return .= "<img src='html/themes/default/images/red.gif'>";
 	}
 	$return .= "</td>";
 	$return .= "<td>";
-	if (@ $result['fatal'] == 1) {
-		$return .= "<b>Needed!!!!</b>";
-	}
 	$return .= "</td>";
 	$return .= "</tr>";
 	echo $return;
 }
 
+/*******************************************************************************
+ END FUNCTIONS
+*******************************************************************************/
 
-#################################################################################
-## end functions
-#################################################################################
+/*******************************************************************************
+ START PAGE
+*******************************************************************************/
+
+// array extensions
+$extensions = array("session", "pcre");
+// array settings
+$settings = array("safe_mode");
+// array binaries
+$binaries = array("grep", "cat", "php", "python", "awk", "du", "wget", "unzip", "cksfv");
+
+// start output
+// form
 if(!isset($_POST['page']) || $_POST['page'] == 1) {
-	# first page, check some basics
-	?>
-	<form action="setup.php" method="post">
-	<table>
-	<tr>
-		<td>
-			<u><b>Step one: Check some Basics</b></u>
-		</td>
-	</tr>
-	<?php
-	display_results("PHP Session Support:", check_extension("session", 1));
-	display_results("PHP PCRE Support:", check_extension("pcre", 1));
-	display_results("Safe Mode:", check_config("safe_mode", 1));
-	?>
-	</table>
-	<br>
+?>
+<form action="setup.php" method="post">
 	<input type="hidden" name="page" value="2">
-	<table border="0" cellspacing="0" cellpadding="0">
-		<tr>
-			<td>Select type of Database:</td>
-			<td><input type="radio" name="db_type" value="mysql" checked="checked" />Mysql</td>
-			<td><input type="radio" name="db_type" value="sqlite" />Sqlite</td>
-		</tr>
-		<tr>
-			<td>Install or just check?</td>
-			<td><input type="radio" name="op" value="1" checked="checked"> only check</td>
-		<!--not used now	<td><input type="radio" name="op" value="2"> install</td> -->
-		</tr>
-		<tr>
-			<td colspan="3" align="center"><input type="submit" value="Next"></td>
-		</tr>
-	</table>
-	</form>
-	<?php
-}
-elseif($_POST['page'] == 2) {
-	?>
-	<form action="setup.php" method="post">
-	<table>
-	<tr>
-		<td>
-			<u><b>Step two: Check some more Settings</b></u>
-		</td>
-	</tr>
-	<?php
-	if($_POST['db_type'] == "mysql") {
-		display_results("PHP MySQL Support:", check_extension("mysql", 1));
-	}
-	elseif($_POST['db_type'] == "sqlite") {
-		display_results("PHP SQLite Support:", check_extension("SQLite", 1));
-	}
-	$osString = php_uname('s');
-	if(isset($osString)) {
-		if(!(stristr($osString, 'linux') === false)) { // linux
-			display_results("check for loadavg:", check_binary("loadavg", 1));
-			display_results("check for netstat:", check_binary("netstat", 1));
-		}
-		elseif(!(stristr($osString, 'bsd') === false)) { // bsd
-			display_results("check for sockstat:", check_binary("sockstat", 1));
-		}
-	}
-	display_results("check for grep:", check_binary("grep", 1));
-	display_results("check for cat:", check_binary("cat", 1));
-	display_results("check for php:", check_binary("php", 1));
-	display_results("check for python:", check_binary("python", 1));
-	display_results("check for awk:", check_binary("awk", 1));
-	display_results("check for du:", check_binary("du", 1));
-	display_results("check for wget:", check_binary("wget", 0));
-	display_results("check for unzip:", check_binary("unzip", 0));
-	display_results("check for cksfv:", check_binary("cksfv", 0));
-	?>
-	</table>
-	<br>
-	<input type="hidden" name="page" value="3">
-	<input type="hidden" name="db_type" value="<?php echo $_POST['db_type'] ?>">
-	<input type="hidden" name="op" value="<?php echo $_POST['op'] ?>">
-	<table border="0" cellspacing="0" cellpadding="0">
-		<tr>
-			<td>Also check Database connection? (optional)</td>
-			<td><input type="radio" name="check_db" value="1" checked="checked"> Don't check</td>
-			<td><input type="radio" name="check_db" value="2"> check</td>
-		</tr>
-		<tr>
-			<td colspan="3" align="center"><input type="submit" value="Next"></td>
-		</tr>
-	</table>
-	</form>
-	<?php
-}
-elseif($_POST['page'] == 3) {
-	if($_POST['check_db'] == 1) {
-	?>
-		<table>
+	<table border="0" cellspacing="0" cellpadding="2">
 		<tr>
 			<td>
-				<u><b>Finished!!!!</b></u>
+				<u><b>Settings:</b></u>
 			</td>
 		</tr>
-		</table>
-	<?php
-	}
-	elseif($_POST['check_db'] == 2) {
-			?>
-	<form action="setup.php" method="post">
-	<table>
-	<tr>
-		<td>
-			<u><b>Step three: Provide Database Informations:</b></u>
-		</td>
-	</tr>
-	<?php
-	if($_POST['db_type'] == "mysql") {
-	?>
+		<tr>
+			<tr>
+				<td>Select type of Database:</td>
+				<td><input type="radio" name="db_type" value="mysql" checked="checked" />MySQL</td>
+				<td><input type="radio" name="db_type" value="sqlite" />SQLite</td>
+			</tr>
+		</tr>
 		<tr>
 			<td>Name of the Database:</td>
 			<td colspan="2"><input type="text" name="db_name"></td>
@@ -240,39 +154,37 @@ elseif($_POST['page'] == 3) {
 			<td>Database Password:</td>
 			<td colspan="2"><input type="password" name="db_pass"></td>
 		</tr>
-	<?php
-	}
-	elseif($_POST['db_type'] == "sqlite") {
-	?>
 		<tr>
-			<td>Path to Database:</td>
-			<td colspan="2"><input type="text" name="db_name"></td>
+			<td colspan="3" align="center"><input type="submit" value="Next"></td>
 		</tr>
-	<?php
-	}
-	?>
-	<input type="hidden" name="page" value="4">
-	<input type="hidden" name="db_type" value="<?php echo $_POST['db_type'] ?>">
-	<input type="hidden" name="op" value="<?php echo $_POST['op'] ?>">
-	<tr>
-		<td colspan="3" align="center"><input type="submit" value="Next"></td>
-	</tr>
 	</table>
-	</form>
-	<?php
-	}
+</form>
+<?php
 }
-elseif($_POST['page'] == 4) {
+elseif($_POST['page'] == 2) {
 ?>
-	<form action="setup.php" method="post">
-	<table>
+<table border="0" cellspacing="0" cellpadding="2">
 	<tr>
 		<td>
-			<u><b>Step four: Check Database</b></u>
+			<u><b>Check PHP settings:</b></u>
 		</td>
 	</tr>
 	<?php
-	if($_POST['db_type'] == "mysql") {
+	foreach($extensions as $extension) {
+		display_results("PHP ".$extension." Support:", check_extension($extension));
+	}
+	foreach($settings as $setting) {
+		display_results("PHP ".$setting.":", check_config($setting));
+	}
+	?>
+	<tr>
+		<td>
+			<u><b>Check database settings:</b></u>
+		</td>
+	</tr>
+	<?php
+	if ($_POST['db_type'] == "mysql") {
+		display_results("PHP MySQL Support:", check_extension("mysql"));
 		$load_ext = get_loaded_extensions();
 		if (in_array("mysql", $load_ext)) {
 			$link = mysql_connect($_POST['db_host'], $_POST['db_user'], $_POST['db_pass']);
@@ -280,33 +192,30 @@ elseif($_POST['page'] == 4) {
 				display_results("check MySQL Connection:", array(
 					'title' => "Successfully connected.",
 					'status' => 1,
-					'fatal' => 0,
 				));
 			}
 			else {
 				display_results("check MySQL Connection:", array(
 					'title' => "Connection failed.",
 					'status' => 0,
-					'fatal' => 1,
 				));
 			}
 			if(mysql_select_db($_POST['db_name'])) {
 				display_results("check MySQL Database:", array(
 					'title' => "Successfully selected Database.",
 					'status' => 1,
-					'fatal' => 0,
 				));
 			}
 			else {
 				display_results("check MySQL Database:", array(
 					'title' => "Selecting Database failed.",
 					'status' => 0,
-					'fatal' => 1,
 				));
 			}
 		}
 	}
-	elseif($_POST['db_type'] == "sqlite") {
+	elseif ($_POST['db_type'] == "sqlite") {
+		display_results("PHP SQLite Support:", check_extension("SQLite"));
 		$load_ext = get_loaded_extensions();
 		if (in_array("SQLite", $load_ext)) {
 			if(is_file($_POST['db_name'])) {
@@ -323,37 +232,43 @@ elseif($_POST['page'] == 4) {
 				display_results("check SQLite Database:", array(
 					'title' => "Database exists.",
 					'status' => 1,
-					'fatal' => 0,
 				));
 			}
 			else {
 				display_results("check SQLite Database:", array(
 					'title' => "No Database exists.",
 					'status' => 0,
-					'fatal' => 1,
 				));
 			}
 		}
 	}
 	?>
-	<input type="hidden" name="page" value="5">
-	<input type="hidden" name="db_type" value="<?php echo $_POST['db_type'] ?>">
-	<input type="hidden" name="op" value="<?php echo $_POST['op'] ?>">
-	<tr>
-		<td colspan="3" align="center"><input type="submit" value="Next"></td>
-	</tr>
-	</table>
-	<?php
-}
-elseif($_POST['page'] == 5) {
-?>
-	<table>
 	<tr>
 		<td>
-			<u><b>Finished!!!!</b></u>
+			<u><b>Check binaries:</b></u>
 		</td>
 	</tr>
-	</table>
+	<?php
+	$osString = php_uname('s');
+	if(isset($osString)) {
+		if(!(stristr($osString, 'linux') === false)) { // linux
+			display_results("check for loadavg:", check_binary("loadavg"));
+			display_results("check for netstat:", check_binary("netstat"));
+		}
+		elseif(!(stristr($osString, 'bsd') === false)) { // bsd
+			display_results("check for sockstat:", check_binary("sockstat"));
+		}
+	}
+	foreach($binaries as $binary) {
+		display_results("check for ".$binary.":", check_binary($binary));
+	}
+	?>
+</table>
 <?php
 }
+
+/*******************************************************************************
+ END PAGE
+*******************************************************************************/
+
 ?>
