ee1cb2afa2bc64232e7d523247fa0e3c8a8dc86f
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-09-22 11:10:46 +0000

    first try to solve the "resetting fields" on upload-field-add. not working as setting value of file-field with javascript is not allowed due to security. (code is correct and "would work" and is in comments.. maybe got another idea...)

--- trunk/html/js/multiup.js
+++ trunk/html/js/multiup.js
@@ -1,11 +1,33 @@
 
 
 // fields
-var formElement = '<input type="File" name="upload_files[]" size="30">';
+var fileCtr = 1;
+var fileAry = new Array();
 
 /**
  * addUploadField
  */
 function addUploadField() {
-	document.getElementById("fileUploadDiv").innerHTML += "<br>\n" + formElement;
+	/*
+	// get old values
+	for (i = 0; i < fileCtr; i++) {
+		id = "file_" + String(i);
+		fileAry[i] = document.getElementById(id).value;
+	}
+	*/
+	// add element
+	addElement  = "<br>\n" + '<input type="File" name="upload_files[]" size="30" id="file_';
+	addElement += String(fileCtr);
+	addElement += '">';
+	document.getElementById("fileUploadDiv").innerHTML += addElement;
+	/*
+	// set values
+	for (i = 0; i < fileCtr; i++) {
+		id = "file_" + String(i);
+		// next op is not allowed (security exception) :
+		document.getElementById(id).value = fileAry[i];
+	}
+	*/
+	// increment field-count
+	fileCtr++;
 }
