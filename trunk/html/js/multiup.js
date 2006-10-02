47f8eb5bc323d2989861da1108725ad4c1fc3533
msn_exploder <msn_exploder@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-10-02 20:39:54 +0000

    minors in multiup.js.

--- trunk/html/js/multiup.js
+++ trunk/html/js/multiup.js
@@ -1,19 +1,13 @@
 /* $Id$ */
 
-// fields
-var formContent = '<br><input type="File" name="upload_files[]" size="40"><span id="fileUploadSpan"></span>';
-
 /**
  * addUploadField
  */
 function addUploadField() {
-	element = document.getElementById("fileUploadSpan");
-	if (typeof(element.outerHTML) != 'undefined') {
-		element.outerHTML = formContent;
-	} else {
-		var range = document.createRange();
-		range.setStartBefore(element);
-		element.parentNode.replaceChild(range.createContextualFragment(formContent), element);
-	}
-	// alert(document.getElementById("fileUploadCell").innerHTML);
+	formContent = document.getElementById("fileUploadCell");
+	var inp  = document.createElement('input');
+	inp.type = 'file';
+	inp.name = 'upload_files[]';
+	inp.size = '40';
+	formContent.appendChild(inp);
 }
