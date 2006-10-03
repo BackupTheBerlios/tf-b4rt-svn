1bb592805eaa134adf804a4788a0bf18d5dd6077
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-10-03 14:38:52 +0000

    revert multiup-changes.

--- trunk/html/js/multiup.js
+++ trunk/html/js/multiup.js
@@ -1,13 +1,19 @@
 /* $Id$ */
 
+// fields
+var formContent = '<br><input type="File" name="upload_files[]" size="40"><span id="fileUploadSpan"></span>';
+
 /**
  * addUploadField
  */
 function addUploadField() {
-	formContent = document.getElementById("fileUploadCell");
-	var inp  = document.createElement('input');
-	inp.type = 'file';
-	inp.name = 'upload_files[]';
-	inp.size = '40';
-	formContent.appendChild(inp);
+	element = document.getElementById("fileUploadSpan");
+	if (typeof(element.outerHTML) != 'undefined') {
+		element.outerHTML = formContent;
+	} else {
+		var range = document.createRange();
+		range.setStartBefore(element);
+		element.parentNode.replaceChild(range.createContextualFragment(formContent), element);
+	}
+	// alert(document.getElementById("fileUploadCell").innerHTML);
 }
