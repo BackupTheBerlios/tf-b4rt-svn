426678a93d9b1bf6bb3953ebc07ebe960f41149a
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-10-04 17:30:51 +0000

    setup : up to database-config-file write.

--- trunk/html/setup.php
+++ trunk/html/setup.php
@@ -23,10 +23,12 @@
 // defines
 define('_NAME', 'torrentflux-b4rt');
 define('_REVISION', array_shift(explode(" ",trim(array_pop(explode(":",'$Revision$'))))));
-define('_VERSION_LOCAL','.version');
+define('_VERSION_LOCAL', '.version');
 define('_VERSION_THIS', trim(getDataFromFile(_VERSION_LOCAL)));
 define('_TITLE', _NAME.' '._VERSION_THIS.' - Setup');
-define('_FILE_THIS',$_SERVER['SCRIPT_NAME']);
+define('_DIR', dirname($_SERVER["SCRIPT_FILENAME"])."/");
+define('_FILE_DBCONF', 'inc/config/config.db.php');
+define('_FILE_THIS', $_SERVER['SCRIPT_NAME']);
 
 // Database-Types
 $databaseTypes = array();
@@ -34,24 +36,59 @@ $databaseTypes['mysql'] = 'mysql_connect';
 $databaseTypes['sqlite'] = 'sqlite_open';
 $databaseTypes['postgres'] = 'pg_connect';
 
-// sql-queries test
-$databaseQueriesTest = array();
+// sql-queries
+$queries = array();
 
+// -----------------------------------------------------------------------------
 // mysql
-$databaseQueriesTest['mysql'] = array();
-array_push($databaseQueriesTest['mysql'], "CREATE TABLE tf_test ( tf_key VARCHAR(255) NOT NULL default '', tf_value TEXT NOT NULL, PRIMARY KEY (tf_key) ) TYPE=MyISAM");
-array_push($databaseQueriesTest['mysql'], "DROP TABLE tf_test");
+// -----------------------------------------------------------------------------
+$cdb = 'mysql';
 
+// sql-queries : Test
+$cqt = 'test';
+$queries[$cqt][$cdb] = array();
+array_push($queries[$cqt][$cdb], "CREATE TABLE tf_test ( tf_key VARCHAR(255) NOT NULL default '', tf_value TEXT NOT NULL, PRIMARY KEY (tf_key) ) TYPE=MyISAM");
+array_push($queries[$cqt][$cdb], "DROP TABLE tf_test");
+
+// sql-queries : Create
+$cqt = 'create';
+$queries[$cqt][$cdb] = array();
+array_push($queries[$cqt][$cdb], "CREATE TABLE tf_test ( tf_key VARCHAR(255) NOT NULL default '', tf_value TEXT NOT NULL, PRIMARY KEY (tf_key) ) TYPE=MyISAM");
+array_push($queries[$cqt][$cdb], "DROP TABLE tf_test");
+
+// -----------------------------------------------------------------------------
 // sqlite
-$databaseQueriesTest['sqlite'] = array();
-array_push($databaseQueriesTest['sqlite'], "CREATE TABLE tf_test ( tf_key VARCHAR(255) NOT NULL default '', tf_value TEXT NOT NULL, PRIMARY KEY (tf_key) )");
-array_push($databaseQueriesTest['sqlite'], "DROP TABLE tf_test");
+// -----------------------------------------------------------------------------
+$cdb = 'sqlite';
 
+// sql-queries : Test
+$cqt = 'test';
+$queries[$cqt][$cdb] = array();
+array_push($queries[$cqt][$cdb], "CREATE TABLE tf_test ( tf_key VARCHAR(255) NOT NULL default '', tf_value TEXT NOT NULL, PRIMARY KEY (tf_key) )");
+array_push($queries[$cqt][$cdb], "DROP TABLE tf_test");
+
+// sql-queries : Create
+$cqt = 'create';
+$queries[$cqt][$cdb] = array();
+array_push($queries[$cqt][$cdb], "CREATE TABLE tf_test ( tf_key VARCHAR(255) NOT NULL default '', tf_value TEXT NOT NULL, PRIMARY KEY (tf_key) )");
+array_push($queries[$cqt][$cdb], "DROP TABLE tf_test");
+
+// -----------------------------------------------------------------------------
 // postgres
-$databaseQueriesTest['postgres'] = array();
-array_push($databaseQueriesTest['postgres'], "CREATE TABLE tf_test ( tf_key VARCHAR(255) NOT NULL DEFAULT '', tf_value TEXT DEFAULT '' NOT NULL, PRIMARY KEY (tf_key) )");
-array_push($databaseQueriesTest['postgres'], "DROP TABLE tf_test");
+// -----------------------------------------------------------------------------
+$cdb = 'postgres';
 
+// sql-queries : Test
+$cqt = 'test';
+$queries[$cqt][$cdb] = array();
+array_push($queries[$cqt][$cdb], "CREATE TABLE tf_test ( tf_key VARCHAR(255) NOT NULL DEFAULT '', tf_value TEXT DEFAULT '' NOT NULL, PRIMARY KEY (tf_key) )");
+array_push($queries[$cqt][$cdb], "DROP TABLE tf_test");
+
+// sql-queries : Create
+$cqt = 'create';
+$queries[$cqt][$cdb] = array();
+array_push($queries[$cqt][$cdb], "CREATE TABLE tf_test ( tf_key VARCHAR(255) NOT NULL DEFAULT '', tf_value TEXT DEFAULT '' NOT NULL, PRIMARY KEY (tf_key) )");
+array_push($queries[$cqt][$cdb], "DROP TABLE tf_test");
 
 // -----------------------------------------------------------------------------
 // Main
@@ -202,6 +239,14 @@ if (isset($_REQUEST["1"])) {                                                    
 		$host = $_REQUEST["db_host"];
 	else
 		$paramsOk = false;
+	if (isset($_REQUEST["db_create"]))
+		$create = true;
+	else
+		$create = false;
+	if (isset($_REQUEST["db_pcon"]))
+		$pcon = "true";
+	else
+		$pcon = "false";
 	switch ($type) {
 		case "mysql":
 		case "postgres":
@@ -217,22 +262,18 @@ if (isset($_REQUEST["1"])) {                                                    
 				$pass = $_REQUEST["db_pass"];
 			else
 				$paramsOk = false;
-			if (isset($_REQUEST["db_pcon"]))
-				$pcon = true;
-			else
-				$pcon = false;
-			if (isset($_REQUEST["db_create"]))
-				$create = true;
-			else
-				$create = false;
 			break;
+		case "sqlite":
+			$name = "";
+			$user = "";
+			$pass = "";
 	}
 	$databaseTestOk = false;
 	$databaseError = "";
 	// create + test
 	if ($paramsOk) {
 		$databaseExists = true;
-		if ($create) {
+		if (($create) && ($type != "sqlite")) {
 			$dbCon = getAdoConnection($type, $host, $user, $pass);
 			if (!$dbCon) {
 				$databaseExists = false;
@@ -261,7 +302,7 @@ if (isset($_REQUEST["1"])) {                                                    
 				$databaseError = "cannot connect to database.";
 			} else {
 				send('<ul>');
-				foreach ($databaseQueriesTest[$type] as $databaseTypeName => $databaseQuery) {
+				foreach ($queries['test'][$type] as $databaseTypeName => $databaseQuery) {
 					send('<li><em>'.$databaseQuery.'</em> : ');
 					$dbCon->Execute($databaseQuery);
 					if ($dbCon->ErrorNo() == 0) {
@@ -286,8 +327,14 @@ if (isset($_REQUEST["1"])) {                                                    
 	if ($databaseTestOk) {
 		send('<font color="green"><strong>Ok</strong></font><br>');
 		send("<h2>Next : Write Config File</h2>");
+		send("Please ensure this script can write to the dir <em>"._DIR."inc/config/</em><p>");
 		send('<form name="setup" action="' . _FILE_THIS . '" method="post">');
 		send('<input type="Hidden" name="db_type" value="'.$type.'">');
+		send('<input type="Hidden" name="db_host" value="'.$host.'">');
+		send('<input type="Hidden" name="db_name" value="'.$name.'">');
+		send('<input type="Hidden" name="db_user" value="'.$user.'">');
+		send('<input type="Hidden" name="db_pass" value="'.$pass.'">');
+		send('<input type="Hidden" name="db_pcon" value="'.$pcon.'">');
 		send('<input type="Hidden" name="15" value="">');
 		send('<input type="submit" value="Continue">');
 	} else {
@@ -311,10 +358,77 @@ if (isset($_REQUEST["1"])) {                                                    
 		send('<input type="submit" value="Back">');
 	}
 	send('</form>');
-
-
-
-
+} elseif (isset($_REQUEST["15"])) {                                             // 15 - Database - config-file
+	sendHead(" - Database");
+	send("<h1>"._TITLE."</h1>");
+	send("<h2>Database - Config-File</h2>");
+	$type = $_REQUEST["db_type"];
+	$host = $_REQUEST["db_host"];
+	$name = $_REQUEST["db_name"];
+	$user = $_REQUEST["db_user"];
+	$pass = $_REQUEST["db_pass"];
+	$pcon = $_REQUEST["db_pcon"];
+	// write file
+	$databaseConfWriteOk = false;
+	$databaseConfWriteError = "";
+	$databaseConfContent = "";
+	writeDatabaseConfig($type, $host, $user, $pass, $name, $pcon);
+	// output
+	if ($databaseConfWriteOk) {
+		send('<font color="green"><strong>Ok</strong></font><br>');
+		send('database-config-file <em>'._DIR._FILE_DBCONF.'</em> written.');
+	} else {
+		send('<font color="red"><strong>Error</strong></font><br>');
+		send($databaseConfWriteError."<p>");
+		send('to perform this step manual paste the following content to the database-config-file <em>'._DIR._FILE_DBCONF.'</em> : <p>');
+		send('<textarea cols="81" rows="33">'.$databaseConfContent.'</textarea>');
+		send("<p>Note : You must write this file before you can continue !");
+	}
+	send("<h2>Next : Create Tables</h2>");
+	sendButton(16);
+} elseif (isset($_REQUEST["16"])) {                                             // 16 - Database - table-creation
+	sendHead(" - Database");
+	send("<h1>"._TITLE."</h1>");
+	send("<h2>Database - Create Tables</h2>");
+	if (is_file(_FILE_DBCONF)) {
+		require_once(_FILE_DBCONF);
+		$databaseTableCreation = false;
+		$databaseError = "";
+		$dbCon = getAdoConnection($cfg["db_type"], $cfg["db_host"], $cfg["db_user"], $cfg["db_pass"], $cfg["db_name"]);
+		if (!$dbCon) {
+			$databaseTableCreation = false;
+			$databaseError = "cannot connect to database.";
+		} else {
+			send('<ul>');
+			foreach ($queries['create'][$cfg["db_type"]] as $databaseTypeName => $databaseQuery) {
+				send('<li><em>'.$databaseQuery.'</em> : ');
+				$dbCon->Execute($databaseQuery);
+				if ($dbCon->ErrorNo() == 0) {
+					send('<font color="green">Ok</font></li>');
+					$databaseTableCreation = true;
+				} else { // damn there was an error
+					send('<font color="red">Error</font></li>');
+					// close ado-connection
+					$dbCon->Close();
+					$databaseTableCreation = false;
+					break;
+				}
+			}
+			send('</ul>');
+		}
+		if ($databaseTableCreation) {
+			send('<font color="green"><strong>Ok</strong></font><br>');
+			send('database-tables created.');
+			send("<h2>Next : Insert Data</h2>");
+			sendButton(17);
+		} else {
+			send('<font color="red"><strong>Error</strong></font><br>');
+			send($databaseError."<p>");
+		}
+	} else {
+		send('<font color="red"><strong>Error</strong></font><br>');
+		send('database-config-file <em>'._DIR._FILE_DBCONF.'</em> missing. setup cannot continue.');
+	}
 } elseif (isset($_REQUEST["2"])) {                                              // 2 - Configuration
 	sendHead(" - Configuration");
 	send("<h1>"._TITLE."</h1>");
@@ -351,9 +465,79 @@ exit();
 // functions
 // -----------------------------------------------------------------------------
 
+
+
+/**
+ * write the db-conf file.
+ *
+ * @param $type
+ * @param $host
+ * @param $user
+ * @param $pass
+ * @param $name
+ * @param $pcon
+ * @return boolean
+ */
+function writeDatabaseConfig($type, $host, $user, $pass, $name, $pcon) {
+	global $databaseConfWriteOk, $databaseConfWriteError, $databaseConfContent;
+	$databaseConfContent = '<?php
+
+/*******************************************************************************
+
+ LICENSE
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License (GPL)
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ To read the license please visit http://www.gnu.org/copyleft/gpl.html
+
+*******************************************************************************/
+
+/******************************************************************************/
+// YOUR DATABASE CONNECTION INFORMATION
+/******************************************************************************/
+$cfg["db_type"] = "'.$type.'";  // Databae-Type : mysql/sqlite/postgres
+$cfg["db_host"] = "'.$host.'";  // Database host computer name or IP
+$cfg["db_name"] = "'.$name.'";  // Name of the Database
+$cfg["db_user"] = "'.$user.'";  // Username for Database
+$cfg["db_pass"] = "'.$pass.'";  // Password for Database
+$cfg["db_pcon"] = '.$pcon.';  // Persistent Connection enabled : true/false
+/******************************************************************************/
+
+?>';
+	$configFile = false;
+	$configFile = @fopen(_DIR._FILE_DBCONF, "w");
+	if (!$configFile) {
+		$databaseConfWriteOk = false;
+		$databaseConfWriteError = "cannot open config-file <em>"._DIR._FILE_DBCONF."</em> for writing.";
+		return false;
+	}
+	$result = @fwrite($configFile, $databaseConfContent);
+	@fclose($configFile);
+	if ($result === false) {
+		$databaseConfWriteOk = false;
+		$databaseConfWriteError = "cannot write content to config-file <em>"._DIR._FILE_DBCONF."</em>.";
+		return false;
+	}
+	$databaseConfWriteOk = true;
+	return true;
+}
+
 /**
  * get a ado-connection to our database.
  *
+ * @param $type
+ * @param $host
+ * @param $user
+ * @param $pass
+ * @param $name
  * @return database-connection or false on error
  */
 function getAdoConnection($type, $host, $user, $pass, $name = "") {
