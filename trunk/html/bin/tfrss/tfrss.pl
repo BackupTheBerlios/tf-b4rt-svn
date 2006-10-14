e0b12c228cdd7512f97e0ab62b26cc4c69085e10
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-10-14 14:55:38 +0000

    tfrss : * op for version * op for check * more

--- trunk/html/bin/tfrss/tfrss.pl
+++ trunk/html/bin/tfrss/tfrss.pl
@@ -21,60 +21,119 @@
 #                                                                              #
 #                                                                              #
 ################################################################################
-#
-# tfrss.pl is a simple script to download a torrent rss feed and download
-# torrents that match regular expressions.
-#
+#                                                                              #
+# tfrss.pl is a simple script to download a torrent rss feed and download      #
+# torrents that match regular expressions.                                     #
+#                                                                              #
+# tfrss.pl is based on TFRSS v0.1 by Chris Craig (chris@chriscraig.net)        #
+#                                                                              #
 ################################################################################
-#
-# tfrss.pl is based on TFRSS v0.1 written by Chris Craig (chris@chriscraig.net)
-#
+#                                                                              #
+#  Requirements :                                                              #
+#   * LWP::Simple  ( perl -MCPAN -e "install LWP::Simple" )                    #
+#   * XML::Simple  ( perl -MCPAN -e "install XML::Simple" )                    #
+#                                                                              #
 ################################################################################
+use strict;
+use warnings;
+#
+use XML::Simple;
+use LWP::Simple;
+################################################################################
+
+# arg-vars
+my ($PATH_TFLUX, $PATH_SAVE, $RSS_URL);
+
+# Internal Vars
+my ($VERSION, $DIR, $PROG, $EXTENSION, $USAGE);
+
+#-------------------------------------------------------------------------------
+# Main
+#-------------------------------------------------------------------------------
+
+# init some vars
+$VERSION =
+	do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d"."%02d" x $#r, @r };
+($DIR=$0) =~ s/([^\/\\]*)$//;
+($PROG=$1) =~ s/\.([^\.]*)$//;
+$EXTENSION=$1;
+
+# check args
+my $argCount = scalar(@ARGV);
+if (($argCount != 1) && ($argCount != 3)) {
+	printUsage();
+	exit;
+}
+
+# ops
+if ($argCount == 1) {
+	SWITCH: {
+		$_ = shift @ARGV;
+
+		/check/ && do { # --- check ---
+			check();
+			exit;
+		};
+		/.*(version|-v).*/ && do { # --- version ---
+			printVersion();
+			exit;
+		};
+		/.*(help|-h).*/ && do { # --- help ---
+			printUsage();
+			exit;
+		};
+		printUsage();
+		exit;
+	}
+}
+
+# init vars
+$PATH_TFLUX = shift @ARGV;
+$PATH_SAVE = shift @ARGV;
+$RSS_URL = shift @ARGV;
+
+
+exit;
 
 
 ###############################################################################
-#
 # config
-#
 ###############################################################################
 
 # The list of regular expressions (1 regex per line)
-$filters = "/path/to/tfrss/regex.dat";
+my $filters = "/path/to/tfrss/regex.dat";
 
 # The URL of the rss feed
-$rssFeed = "http://myfeed.com/feeds/thefeed/";
+my $rssFeed = "http://myfeed.com/feeds/thefeed/";
 
 # The location to save torrents (should be the same as your watch folder in
 # TorrentFlux
-$saveLocation = "/path/to/watch/directory/";
+my $saveLocation = "/path/to/watch/directory/";
 
 # History File Location
-$historyFile = "/path/to/tfrss/history.log";
+my $historyFile = "/path/to/tfrss/history.log";
 
 # DO NOT EDIT BELOW THIS POINT UNLESS YOU KNOW WHAT YOU'RE DOING!
 ################################################################################
 
-#includes
-use XML::Simple;
-use LWP::Simple;
+
+# Create the xml object
+my $rss = new XML::Simple;
+
+# Parse the xml object
+my $data = $rss->XMLin(get($rssFeed));
 
 # Open the file with the list of regular expressions
 open(FILTERS,$filters);
 
-# Create the xml object
-$rss = new XML::Simple;
-
-# Parse the xml object
-$data = $rss->XMLin(get($rssFeed));
-
 # Loop through all of the regular expressions
 while(<FILTERS>){
 	chomp;
-	$filter = $_;
+	my $filter = $_;
 	print "*****$filter*****\n";
 
 	# compare the filter to each torrent in the xml doc
-	foreach $torrent (@{$data->{channel}->{item}}){
+	foreach my $torrent (@{$data->{channel}->{item}}){
 
 		# if we have a match, save torrent file
 
@@ -85,7 +144,7 @@ while(<FILTERS>){
 			open(HISTORY,$historyFile);
 
 			# Set the match flag to false
-			$match = 0;
+			my $match = 0;
 
 			#Read through the history file to see if we've already
 			#downloaded this torrent before.
@@ -119,3 +178,69 @@ while(<FILTERS>){
 }
 
 close FILTERS;
+
+
+#===============================================================================
+# Subs
+#===============================================================================
+
+#------------------------------------------------------------------------------#
+# Sub: check                                                                   #
+# Arguments: Null                                                              #
+# Returns: info on system requirements                                         #
+#------------------------------------------------------------------------------#
+sub check {
+	print "checking requirements...\n";
+	# 1. perl-modules
+	print "1. perl-modules\n";
+	my @mods = ('LWP::Simple', 'XML::Simple');
+	foreach my $mod (@mods) {
+		if (eval "require $mod")  {
+			print " - ".$mod."\n";
+			next;
+		} else {
+			print "Error : cant load module ".$mod."\n";
+			# Turn on Autoflush;
+			$| = 1;
+			print "Should we try to install the module with CPAN ? (y|n) ";
+			my $answer = "";
+			chomp($answer=<STDIN>);
+			$answer = lc($answer);
+			if ($answer eq "y") {
+				exec('perl -MCPAN -e "install '.$mod.'"');
+			}
+			exit;
+		}
+	}
+	# done
+	print "done.\n";
+}
+
+#------------------------------------------------------------------------------#
+# Sub: printVersion                                                            #
+# Arguments: Null                                                              #
+# Returns: Version Information                                                 #
+#------------------------------------------------------------------------------#
+sub printVersion {
+	print $PROG.".".$EXTENSION." Version ".$VERSION."\n";
+}
+
+#-------------------------------------------------------------------------------
+# Sub: printUsage
+# Parameters:	-
+# Return:		-
+#-------------------------------------------------------------------------------
+sub printUsage {
+	print <<"USAGE";
+$PROG.$EXTENSION (Revision $VERSION)
+
+Usage: $PROG.$EXTENSION tflux-path save-location rss-feed-url
+
+Example:
+$PROG.$EXTENSION /usr/local/torrent /save/location http://www.example.com/feed.xml
+
+USAGE
+
+}
+
+# EOF
