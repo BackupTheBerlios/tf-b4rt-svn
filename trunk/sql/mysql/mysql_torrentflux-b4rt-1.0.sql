fa91344ba3fac54646e82cf531232cb1529a1126
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-10-14 21:03:16 +0000

    sql-files : added settings for Rssad-module

--- trunk/sql/mysql/mysql_torrentflux-b4rt-1.0.sql
+++ trunk/sql/mysql/mysql_torrentflux-b4rt-1.0.sql
@@ -286,19 +286,22 @@ INSERT INTO tf_settings VALUES ('downloadhosts','0');
 INSERT INTO tf_settings VALUES ('pagetitle','torrentflux-b4rt');
 INSERT INTO tf_settings VALUES ('fluxd_dbmode','php');
 INSERT INTO tf_settings VALUES ('fluxd_loglevel','0');
-INSERT INTO tf_settings VALUES ('fluxd_Qmgr_enabled','0');
 INSERT INTO tf_settings VALUES ('fluxd_Fluxinet_enabled','0');
+INSERT INTO tf_settings VALUES ('fluxd_Qmgr_enabled','0');
+INSERT INTO tf_settings VALUES ('fluxd_Rssad_enabled','0');
 INSERT INTO tf_settings VALUES ('fluxd_Watch_enabled','0');
-INSERT INTO tf_settings VALUES ('fluxd_Clientmaint_enabled','0');
 INSERT INTO tf_settings VALUES ('fluxd_Trigger_enabled','0');
+INSERT INTO tf_settings VALUES ('fluxd_Clientmaint_enabled','0');
+INSERT INTO tf_settings VALUES ('fluxd_Fluxinet_port','3150');
 INSERT INTO tf_settings VALUES ('fluxd_Qmgr_interval','15');
 INSERT INTO tf_settings VALUES ('fluxd_Qmgr_maxTotalTorrents','5');
 INSERT INTO tf_settings VALUES ('fluxd_Qmgr_maxUserTorrents','2');
-INSERT INTO tf_settings VALUES ('fluxd_Fluxinet_port','3150');
-INSERT INTO tf_settings VALUES ('fluxd_Watch_jobs','');
+INSERT INTO tf_settings VALUES ('fluxd_Rssad_interval','1800');
+INSERT INTO tf_settings VALUES ('fluxd_Rssad_jobs','');
 INSERT INTO tf_settings VALUES ('fluxd_Watch_interval','120');
-INSERT INTO tf_settings VALUES ('fluxd_Clientmaint_interval','600');
+INSERT INTO tf_settings VALUES ('fluxd_Watch_jobs','');
 INSERT INTO tf_settings VALUES ('fluxd_Trigger_interval','600');
+INSERT INTO tf_settings VALUES ('fluxd_Clientmaint_interval','600');
 
 --
 -- tf_settings_dir
