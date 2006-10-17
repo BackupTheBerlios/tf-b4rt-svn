1139adb94f38ef7b731e1dc293d6804fe8f30a88
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-10-17 09:48:30 +0000

    old : minor in db-error

--- branches/torrentflux_2.1-b4rt/html/db.php
+++ branches/torrentflux_2.1-b4rt/html/db.php
@@ -55,7 +55,7 @@ function getdb() {
 	$db = @ ADONewConnection($dsn);
 	// check connection
 	if (!$db)
-		showErrorPage('Could not connect to database :<br>Check your database settings in the config.db.php file.');
+		showErrorPage('Could not connect to database.<br>Check your database settings in the config.db.php file.');
 	// return db-connection
 	return $db;
 }
