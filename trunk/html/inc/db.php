ce83c55cdb935b6f8eceff3d96c156793c727f64
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-10-17 09:46:51 +0000

    dont print out any var-info on db-error

--- trunk/html/inc/db.php
+++ trunk/html/inc/db.php
@@ -54,7 +54,7 @@ function getdb() {
 	$db = @ ADONewConnection($dsn);
 	// check connection
 	if (!$db)
-		showErrorPage('Could not connect to database :<br><em>'.$cfg["db_host"].'</em><br>Check your database settings in the config.db.php file.');
+		showErrorPage('Could not connect to database :<br>Check your database settings in the config.db.php file.');
 	// return db-connection
 	return $db;
 }
