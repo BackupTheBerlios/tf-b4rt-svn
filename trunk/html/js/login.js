a13708d6bfc3617ab4321c18734de163dcdf3ecc
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-10-25 14:44:22 +0000

    form-login a bit nicer

--- trunk/html/js/login.js
+++ trunk/html/js/login.js
@@ -1,6 +1,5 @@
 /* $Id$ */
-
-function loginvalidate() {
+function loginSubmit() {
 	msg = "";
 	pass = document.theForm.iamhim.value;
 	user = document.theForm.username.value;
@@ -18,5 +17,17 @@ function loginvalidate() {
 	if (msg != "") {
 		alert("Check the following:\n\n" + msg);
 		return false;
+	}
+	var loginDivForm = document.getElementById("login_form");
+	if (loginDivForm != null) {
+		if (navigator.appName == "Microsoft Internet Explorer" && navigator.userAgent.indexOf("MSIE 7") == -1)
+			loginDivForm.style.top = document.documentElement.scrollTop;
+		loginDivForm.style.display = "none";
+	}
+	var loginDivAction = document.getElementById("login_in_progress");
+	if (loginDivAction != null) {
+		if (navigator.appName == "Microsoft Internet Explorer" && navigator.userAgent.indexOf("MSIE 7") == -1)
+			loginDivAction.style.top = document.documentElement.scrollTop;
+		loginDivAction.style.display = "block";
 	}
 }
