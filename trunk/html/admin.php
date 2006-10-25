1dd37b1bad8f6aff21cbfc1dcb7e0bea93560e43
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-10-25 15:42:30 +0000

    fixed a bug in edit-links (was not working at all)

--- trunk/html/admin.php
+++ trunk/html/admin.php
@@ -249,15 +249,16 @@ switch ($op) {
 
 	case "editLink":
 		$lid = getRequestVar('lid');
-		$editLink = getRequestVar('editLink');
-		$editSite = getRequestVar('editSite');
-		if (!empty($newLink)){
-			if(strpos($newLink, "http://" ) !== 0 && strpos($newLink, "https://" ) !== 0 && strpos($newLink, "ftp://" ) !== 0)
+		$newLink = getRequestVar('editLink');
+		$newSite = getRequestVar('editSite');
+		if(!empty($newLink)){
+			if(strpos($newLink, "http://" ) !== 0 && strpos($newLink, "https://" ) !== 0 && strpos($newLink, "ftp://" ) !== 0){
 				$newLink = "http://".$newLink;
+			}
 			empty($newSite) && $newSite = $newLink;
-			$oldLink=getLink($lid);
-			$oldSite=getSite($lid);
-			alterLink($lid,$newLink,$newSite);
+			$oldLink = getLink($lid);
+			$oldSite = getSite($lid);
+			alterLink($lid, $newLink, $newSite);
 			AuditAction($cfg["constants"]["admin"], "Change Link: ".$oldSite." [".$oldLink."] -> ".$newSite." [".$newLink."]");
 		}
 		header("location: admin.php?op=editLinks");
