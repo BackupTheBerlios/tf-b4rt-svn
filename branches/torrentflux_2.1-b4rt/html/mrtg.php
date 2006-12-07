d32f0910d5df70e296424d24a55babc9f95fa368
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-12-07 12:54:40 +0000

    old : mrtg.php : print a message when no graphs are found.

--- branches/torrentflux_2.1-b4rt/html/mrtg.php
+++ branches/torrentflux_2.1-b4rt/html/mrtg.php
@@ -27,55 +27,61 @@ include_once("functions.php");
 
 // request-vars
 $mrtgTarget = getRequestVar('mrtg_target');
-if($mrtgTarget == '')
-  $mrtgTarget = _DEFAULT_TARGET;
+if ($mrtgTarget == '')
+	$mrtgTarget = _DEFAULT_TARGET;
+
+// content-var
+$htmlGraph = "";
 
 // get list of available targets
 $mrtgTargets = null;
 $htmlTargets = "";
-if ($dirHandle = opendir('./mrtg')) {
-  $htmlTargets .= '<table width="740" border="0" cellpadding="0" cellspacing="0"><tr><td align="center">';
-  $htmlTargets .= '<form name="targetSelector" action="'.$_SERVER['SCRIPT_NAME'].'" method="post">';
-  $htmlTargets .= '<select name="mrtg_target" size="1" onChange="submit();">';
-  $idx = 0;
-  while (false !== ($file = readdir($dirHandle))) {
-    if( preg_match("/.*inc/i", $file) ) {
-      $mrtgTargets[$idx] = $file;
-      $tempAry = explode('.',$file);
-      $targetName = array_shift($tempAry);
-      $htmlTargets .= '<option value="'.$targetName.'"';
-      if ($mrtgTarget == $targetName)
-        $htmlTargets .= ' selected';
-      $htmlTargets .= '>'.$targetName.'</option>';
-      $idx++;
-    }
-  }
-  closedir($dirHandle);
-  $htmlTargets .= '</select><input type="submit" value="Change Graph">';
-  $htmlTargets .= '</form>';
-  $htmlTargets .= '</td></tr></table>'."\n";
+$htmlTargetsCount = 0;
+if ($dirHandle = @opendir('./mrtg')) {
+	$htmlTargets .= '<table width="740" border="0" cellpadding="0" cellspacing="0"><tr><td align="center">';
+	$htmlTargets .= '<form name="targetSelector" action="'.$_SERVER['SCRIPT_NAME'].'" method="post">';
+	$htmlTargets .= '<select name="mrtg_target" size="1" onChange="submit();">';
+	$idx = 0;
+	while (false !== ($file = readdir($dirHandle))) {
+		if (preg_match("/.*inc/i", $file) ) {
+			$htmlTargetsCount++;
+			$mrtgTargets[$idx] = $file;
+			$tempAry = explode('.',$file);
+			$targetName = array_shift($tempAry);
+			$htmlTargets .= '<option value="'.$targetName.'"';
+			if ($mrtgTarget == $targetName)
+				$htmlTargets .= ' selected';
+			$htmlTargets .= '>'.$targetName.'</option>';
+			$idx++;
+		}
+	}
+	closedir($dirHandle);
+	$htmlTargets .= '</select><input type="submit" value="Change Graph">';
+	$htmlTargets .= '</form>';
+	$htmlTargets .= '</td></tr></table>'."\n";
 }
+if ($htmlTargetsCount == 0)
+	$htmlGraph = "<br><p><strong>No Graphs found.</strong></p>";
 
 // get content
-$htmlGraph = "";
 $filename = "./mrtg/".$mrtgTarget.".inc";
 if (file_exists($filename)) {
-  $fileHandle = @fopen($filename, "r");
-  while (!feof($fileHandle))
-    $htmlGraph .= @fgets($fileHandle, 4096);
-  @fclose ($fileHandle);
-  // we are only interested in the "real" content
-  $tempAry = explode("_CONTENT_BEGIN_", $htmlGraph);
-  $tempVar = array_pop($tempAry);
-  $tempAry = explode("_CONTENT_END_", $tempVar);
-  $htmlGraph = array_shift($tempAry);
-  // rewrite image-links
-  $htmlGraph = preg_replace('/(.*")(.*)(png".*)/i', '${1}mrtg/${2}${3}', $htmlGraph);
+	$fileHandle = @fopen($filename, "r");
+	while (!feof($fileHandle))
+		$htmlGraph .= @fgets($fileHandle, 4096);
+	@fclose ($fileHandle);
+	// we are only interested in the "real" content
+	$tempAry = explode("_CONTENT_BEGIN_", $htmlGraph);
+	$tempVar = array_pop($tempAry);
+	$tempAry = explode("_CONTENT_END_", $tempVar);
+	$htmlGraph = array_shift($tempAry);
+	// rewrite image-links
+	$htmlGraph = preg_replace('/(.*")(.*)(png".*)/i', '${1}mrtg/${2}${3}', $htmlGraph);
 }
 
 // render page content
 DisplayHead(_ID_MRTG);
-if ((count($mrtgTargets)) > 0)
+if ($htmlTargetsCount > 0)
   echo $htmlTargets;
 echo '<div align="center" id="BodyLayer" name="BodyLayer" style="border: thin solid ';
 echo $cfg["main_bgcolor"];
