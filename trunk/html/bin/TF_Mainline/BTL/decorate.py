1566460e36d04de05f385d4aac3da204492924f0
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2006-12-07 18:18:17 +0000

    mainline : deleted BitTorrent 5.0.2

--- trunk/html/bin/TF_Mainline/BTL/decorate.py
+++
@@ -1,9 +0,0 @@
-# usage:
-#
-# o.method = decorate_func(somefunc, o.method)
-
-def decorate_func(new, old):
-    def runner(*a, **kw):
-        new(*a, **kw)
-        return old(*a, **kw)
-    return runner
