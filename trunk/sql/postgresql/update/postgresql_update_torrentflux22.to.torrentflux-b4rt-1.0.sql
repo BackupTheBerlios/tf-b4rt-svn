9940965f6dc4f15fde66f0b210397fabcae467f3
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2007-01-01 23:23:43 +0000

    nzb : added setting bin_uudeview to ssql-files

--- trunk/sql/postgresql/update/postgresql_update_torrentflux22.to.torrentflux-b4rt-1.0.sql
+++ trunk/sql/postgresql/update/postgresql_update_torrentflux22.to.torrentflux-b4rt-1.0.sql
@@ -207,6 +207,7 @@ INSERT INTO tf_settings VALUES ('bin_wget','/usr/bin/wget');
 INSERT INTO tf_settings VALUES ('bin_unrar','/usr/bin/unrar');
 INSERT INTO tf_settings VALUES ('bin_unzip','/usr/bin/unzip');
 INSERT INTO tf_settings VALUES ('bin_cksfv','/usr/bin/cksfv');
+INSERT INTO tf_settings VALUES ('bin_uudeview','/usr/local/bin/uudeview');
 INSERT INTO tf_settings VALUES ('btclient','tornado');
 INSERT INTO tf_settings VALUES ('btclient_tornado_options','');
 INSERT INTO tf_settings VALUES ('btclient_transmission_bin','/usr/local/bin/transmissioncli');
