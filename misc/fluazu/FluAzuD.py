bd43a6dfd293b0c7b23267787a58459153aeadda
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2007-02-05 20:10:32 +0000

    "fluazu"

---
+++ misc/fluazu/FluAzuD.py
@@ -0,0 +1,201 @@
+################################################################################
+# $Id$
+# $Date$
+# $Revision$
+################################################################################
+#                                                                              #
+# LICENSE                                                                      #
+#                                                                              #
+# This program is free software; you can redistribute it and/or                #
+# modify it under the terms of the GNU General Public License (GPL)            #
+# as published by the Free Software Foundation; either version 2               #
+# of the License, or (at your option) any later version.                       #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# To read the license please visit http://www.gnu.org/copyleft/gpl.html        #
+#                                                                              #
+#                                                                              #
+################################################################################
+# standard
+import time
+from time import time, strftime, sleep
+# dopal
+from dopal.main import make_connection
+from dopal.errors import LinkError
+################################################################################
+
+class FluAzuD(object):
+
+    """ -------------------------------------------------------------------- """
+    """ __init__                                                             """
+    """ -------------------------------------------------------------------- """
+    def __init__(self):
+        self.state = 1
+        self.running = 1
+        self.errors = []
+        # flux-settings
+        self.flu_path = ''
+        # azu-settings
+        self.azu_host = '127.0.0.1'
+        self.azu_port = 6884
+        self.azu_secure = False
+        self.azu_user = ''
+        self.azu_pass = ''
+        # dopal
+        self.connection_details = {}
+        self.connection = None
+        self.interface = None
+        self.dm = None
+
+    """ -------------------------------------------------------------------- """
+    """ run                                                                  """
+    """ -------------------------------------------------------------------- """
+    def run(self, path, host, port, secure, username, password):
+
+        # dump vars
+        print "path: %s" % path
+        print "host: %s" % host
+        print "port: %s" % port
+        print "secure: %s" % secure
+        print "user: %s" % username
+        print "pass: %s" % password
+
+        # set vars
+        self.flu_path = path
+        self.azu_host = host
+        self.azu_port = int(port)
+        if secure == 1:
+            self.azu_secure = True
+        else:
+            self.azu_secure = False
+        self.azu_user = username
+        self.azu_pass = password
+
+        # set connection details
+        self.connection_details['host'] = self.azu_host
+        self.connection_details['port'] = self.azu_port
+        self.connection_details['secure'] = self.azu_secure
+        if len(self.azu_user) > 0:
+            self.connection_details['user'] = self.azu_user
+            self.connection_details['password'] = self.azu_pass
+
+        # initialize
+        self.initialize()
+
+        # check all dopal-objects
+        if self.connection is None or self.dm is None or self.dm is None:
+            print "there were problems, not starting up daemon-mainloop."
+            return 0
+
+        # main
+        self.main()
+
+    """ -------------------------------------------------------------------- """
+    """ main                                                                 """
+    """ -------------------------------------------------------------------- """
+    def main(self):
+
+        # main-loop
+        while self.running > 0:
+
+            # check if connection still valid
+            if not self.checkConnection():
+                return 0
+
+            # inner loop
+            for i in range(5):
+
+                print "%s (%s)" % (self.flu_path, str(i))
+
+                # sleep
+                sleep(1)
+
+            # print downloads
+            downloads = self.dm.getDownloads()
+            for download in downloads:
+                print "* %s" % str(download)
+                print "  %s" % str(download.getTorrentFileName())
+                torrent = download.getTorrent()
+                print "  %s" % str(torrent.getName())
+                print "  getState: %s" % str(download.getState())
+                print "  getSize: %s" % str(torrent.getSize())
+                stats = download.getStats()
+                print "  getUploaded: %s" % str(stats.getUploaded())
+                print "  getDownloaded: %s" % str(stats.getDownloaded())
+                print "  getUploadAverage: %s" % str(stats.getUploadAverage())
+                print "  getDownloadAverage: %s" % str(stats.getDownloadAverage())
+
+        # return
+        return 1
+
+    """ -------------------------------------------------------------------- """
+    """ initialize                                                           """
+    """ -------------------------------------------------------------------- """
+    def initialize(self):
+
+        # connect
+        self.connection = make_connection(**self.connection_details)
+        self.connection.is_persistent_connection = True
+        try:
+            self.interface = self.connection.get_plugin_interface()
+        except LinkError, error:
+            self.interface = None
+            connection_error = error
+        else:
+            connection_error = None
+        if connection_error is None:
+            print "Connected to %s" % self.azu_host
+        else:
+            print "Error getting plugin interface object - could not connect to Azureus, error:\n %s" % connection_error.to_error_string()
+
+        # print azureus version
+        print "Azureus-Version: " + str(self.connection.get_azureus_version())
+
+        # download-manager
+        self.dm = self.interface.getDownloadManager()
+        if self.dm is None:
+            print "Error getting plugin Download-Manager object"
+            return 0
+
+    """ -------------------------------------------------------------------- """
+    """ checkConnection                                                      """
+    """ -------------------------------------------------------------------- """
+    def checkConnection(self):
+
+        # con valid
+        if self.connection.is_connection_valid():
+            return True
+
+        # con not valid
+        else:
+
+            # establish con
+            try:
+                self.connection.establish_connection(True)
+                print "established connection to " + self.azu_host
+            except:
+                print "Error establishing connection to " + self.azu_host
+                return False
+
+            # interface
+            try:
+                self.interface = self.connection.get_plugin_interface()
+            except LinkError, error:
+                self.interface = None
+            if self.interface is None:
+                print "Error getting plugin interface object"
+                return False
+
+            # download-manager
+            self.dm = None
+            self.dm = self.interface.getDownloadManager()
+            if self.dm is None:
+                print "Error getting plugin Download-Manager object"
+                return False
+            else:
+                return True
+
--- misc/fluazu/fluazu.py
+++ misc/fluazu/fluazu.py
@@ -30,120 +30,44 @@
 #     http://azureus.sourceforge.net/plugin_details.php?plugin=xml_http_if)    #
 #                                                                              #
 ################################################################################
-import commands, os, re, sys
-from os import popen, getpid, remove
-from sys import argv, stdout
-from time import time, strftime
-import thread, threading, time
+# standard
+import sys
+from sys import argv, exit
+# fluazu
+from FluAzuD import FluAzuD
 ################################################################################
 
-################################################################################
-# fields                                                                       #
-################################################################################
 # version
 version_fluazu = '0.1'
-# azu
-azu_host = '127.0.0.1'
-azu_port = 6884
-azu_secure = False
-azu_user = ''
-azu_pass = ''
 
-################################################################################
-# methods                                                                      #
-################################################################################
-
-def run(host, port, secure, username, password):
-
-    # debug
-    print "host : " + host
-    print "port : " + port
-    print "secure : " + secure
-    print "user : " + username
-    print "pass : " + password
-
-    # set vars
-    azu_host = host
-    azu_port = int(port)
-    if secure == 1:
-        azu_secure = True
-    else:
-        azu_secure = False
-    azu_user = username
-    azu_pass = password
-
-    # set connection details
-    connection_details = {}
-    connection_details['host'] = azu_host
-    connection_details['port'] = azu_port
-    connection_details['secure'] = azu_secure
-    if len(azu_user) > 0:
-        connection_details['user'] = azu_user
-        connection_details['password'] = azu_pass
-
-    # connect
-    from dopal.main import make_connection
-    connection = make_connection(**connection_details)
-    connection.is_persistent_connection = True
-    from dopal.errors import LinkError
-    try:
-        interface = connection.get_plugin_interface()
-    except LinkError, error:
-        interface = None
-        connection_error = error
-    else:
-        connection_error = None
-    from dopal import __version_str__
-    print  "DOPAL %s" % __version_str__
-    if connection_error is None:
-        print "Connected"
-    else:
-        print "Error getting plugin interface object - could not connect to Azureus, error:\n %s" % connection_error.to_error_string()
-
-    # version
-    print "Azureus-Version : " + str(connection.get_azureus_version())
-
-    # download-manager
-    dm = interface.getDownloadManager()
-
-    # print downloads
-    downloads = dm.getDownloads()
-    for download in downloads:
-        print "* " + str(download)
-        print "  " + str(download.getTorrentFileName())
-        torrent = download.getTorrent()
-        print "  " + str(torrent.getName())
-        print "  getState: " + str(download.getState())
-        print "  getSize: " + str(torrent.getSize())
-        stats = download.getStats()
-        print "  getUploadAverage: " + str(stats.getUploadAverage())
-        print "  getDownloadAverage: " + str(stats.getDownloadAverage())
-        print "  getUploaded: " + str(stats.getUploaded())
-        print "  getDownloaded: " + str(stats.getDownloaded())
-
-    # exit
-    sys.exit(0)
-
-################################################################################
-# main                                                                         #
-################################################################################
-
+""" ------------------------------------------------------------------------ """
+""" main                                                                     """
+""" ------------------------------------------------------------------------ """
 if __name__ == '__main__':
 
     # version
     if argv[1:] == ['--version']:
-        print version_fluazu
-        sys.exit(0)
+        print "fluazu-version: %s" % version_fluazu
+        from dopal import __version_str__
+        print "DOPAL-version: %s" % __version_str__
+        exit(0)
 
     # check argv-length
-    if len(argv) < 6:
+    if len(argv) < 7:
         print "Error : missing arguments, exiting. \n"
-        sys.exit(0)
+        exit(0)
 
-    # run
-    run(argv[1], argv[2], argv[3], argv[4], argv[5])
+    # run daemon
+    daemon = FluAzuD()
+    exitVal = 0
+    try:
+        exitVal = daemon.run(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6])
+    except KeyboardInterrupt:
+        daemon.running = 0
+        pass
+    except Exception, e:
+        print e
 
-
-
-
+    # exit
+    exit(exitVal)
 
