c146f5fb778a6b806521871495b6f29445b57ccf
munk <munk@81ed43e1-a318-0410-8e7c-889f91f65c99>
2007-02-13 19:42:36 +0000

    Stuff to make google analytics work.

--- misc/website/htdocs/index.php
+++ misc/website/htdocs/index.php
@@ -144,8 +144,24 @@ function printPageFoot() {
 		</div>
 	</div>
 </div>
+<?php googleAnalytics(); ?>
 </body>
 </html>
+<?php
+}
+
+/**
+ * Inserts Google Analytics javascript for tracking and analysis via:
+ * https://www.google.com/analytics/
+ */
+function googleAnalytics(){
+?>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+	<script type="text/javascript">
+_uacct = "UA-1343358-1";
+urchinTracker();
+</script>
 <?php
 }
 
