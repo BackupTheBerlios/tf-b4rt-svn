c9e17f07ea9fd2a324db8037d28d5f6475aa3149
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2007-03-22 00:00:03 +0000

    website: added script to export dynamic content to webserver

---
+++ misc/website/content.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# shell-script to export dynamic content to website
+#
+
+# news-export
+/usr/bin/wget -q -O ~/tf-b4rt/newshtml.tmp 'http://developer.berlios.de/export/projnews.php?group_id=7000&limit=10&flat=1&show_summaries=0' > /dev/null
+/bin/mv -f ~/tf-b4rt/newshtml.tmp /home/groups/tf-b4rt/htdocs/newshtml.txt
+
+# files from svn
+SVNFILES="AUTHORS
+		CHANGES
+		FAQ
+		FEATURES
+		INSTALL
+		README
+		TODO"
+for SVNFILE in $SVNFILES
+do
+	/usr/bin/svn export --quiet --non-interactive http://svn.berlios.de/svnroot/repos/tf-b4rt/trunk/$SVNFILE
+	/bin/mv -f ~/tf-b4rt/$SVNFILE /home/groups/tf-b4rt/htdocs/$SVNFILE
+done
