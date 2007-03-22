beb2d5a6ba2d88bd43a046fc7bb24563774e8dfe
b4rt <b4rt@81ed43e1-a318-0410-8e7c-889f91f65c99>
2007-03-22 01:21:34 +0000

    website: updated content.sh

--- misc/website/content.sh
+++ misc/website/content.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-#
+################################################################################
+# $Id$
+# $Revision$
+# $Date$
+################################################################################
 # shell-script to export dynamic content to website
-#
+################################################################################
 
 # news-export
 /usr/bin/wget -q -O ~/tf-b4rt/newshtml.tmp 'http://developer.berlios.de/export/projnews.php?group_id=7000&limit=10&flat=1&show_summaries=0' > /dev/null
 /bin/mv -f ~/tf-b4rt/newshtml.tmp /home/groups/tf-b4rt/htdocs/newshtml.txt
 
 # files from svn
+cd ~/tf-b4rt/
 SVNFILES="AUTHORS
 		CHANGES
 		FAQ
