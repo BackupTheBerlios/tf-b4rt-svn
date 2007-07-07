7814fdb660eec7a72b062f57a2b615bf87ead2ff
danez <danez@81ed43e1-a318-0410-8e7c-889f91f65c99>
2007-07-07 18:33:44 +0000

    formatBytestokBMBGBTB now also returns filessize in Bytes if size < 1024

--- trunk/html/inc/functions/functions.core.php
+++ trunk/html/inc/functions/functions.core.php
@@ -492,10 +492,12 @@ function formatBytesTokBMBGBTB($inBytes) {
 		return round($inBytes / 1099511627776, 2) . " TB";
 	elseif ($inBytes > 1073741824)
 		return round($inBytes / 1073741824, 2) . " GB";
-	elseif ($inBytes < 1048576)
+	elseif ($inBytes > 1048576)
+		return round($inBytes / 1048576, 1) . " MB";
+	elseif ($inBytes > 1024)
 		return round($inBytes / 1024, 1) . " kB";
 	else
-		return round($inBytes / 1048576, 1) . " MB";
+		return $inBytes . " B";
 }
 
 /**
