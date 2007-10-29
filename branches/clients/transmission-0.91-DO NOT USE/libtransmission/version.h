9e76607efddb6a58abb0ff8f118ed6e3e877344c
danez <danez@81ed43e1-a318-0410-8e7c-889f91f65c99>
2007-10-29 23:55:50 +0000

    vanillia client

---
+++ branches/clients/transmission-0.91-DO NOT USE/libtransmission/version.h
@@ -0,0 +1,5 @@
+#define PEERID_PREFIX         "-TR0910-"
+#define USERAGENT_PREFIX      "0.91"
+#define SVN_REVISION          "3628"
+#define SHORT_VERSION_STRING  "0.91"
+#define LONG_VERSION_STRING   "0.91 (3628)"
